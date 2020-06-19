5479a2e073616cb0a3b24ba2604216f62d99cccf
Paul Brown <paul.brown@bjss.com>
2020-06-19 07:57:33 +0100

    Day 5 solved

--- 2016/day5.py
+++ 2016/day5.py
@@ -3,11 +3,24 @@ import hashlib
 input2 = 'abc'
 input = 'reyedfim'
 output = ''
+output2 = ['-'] * 8
 h = ''
 for i in range(1000000000):
     h = hashlib.md5((input+str(i)).encode('utf-8')).hexdigest() 
     if h.startswith("00000"):
-        output += h[5]
-        if len(output) == 8:
-            break
-print (output)
+        #output += h[5]
+        #if len(output) == 8:
+        #    break
+        print(h)
+        if h[5].isdigit():
+            if int(h[5]) < 8 and output2[int(h[5])] == '-':
+                output2[int(h[5])] = h[6]
+                print(output2)
+            
+            if "-" not in "".join(output2):
+                break
+        
+        
+print ("".join(output2))
+
+# e635d627
