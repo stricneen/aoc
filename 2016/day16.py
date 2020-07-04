cd2770f19eae1e6264262e7636432da6b1745a68
Paul Brown <paul.brown@bjss.com>
2020-07-04 12:51:42 +0100

    t2

--- 2016/day16.py
+++ 2016/day16.py
@@ -1,7 +1,5 @@
 start = '00111101111101000'
 
-
-
 # Call the data you have at this point "a".
 # Make a copy of "a"; call this copy "b".
 # Reverse the order of the characters in "b".
@@ -10,28 +8,31 @@ start = '00111101111101000'
 
 def step(data):
     b = ''
-    while len(data) % 2 == 0:
-        data = partchecksum(data)
+    # while len(data) % 2 == 0:
+    #     print(len(data))
+    # data = partchecksum(data)
     
     for c in data:
         d = '0' if c == '1' else '1'
         b = d + b
     return data + '0' + b
 
-def create(data, ln):
+def create(data, sizereq, current):
     s = step(data)
-    # print(len(s))
-    # print(s)
-    if len(s) >= ln:
-        return s[0:ln]
+    
+    current = (current * 2) + 1    
+    print(current, sizereq)
+    if current >= sizereq:
+        return s[0:sizereq]
     else:
-        # print(35651584 - len(s))
-        return create(s, ln)
-
-
+        return create(s, sizereq, current)
 
 def checksum(data):
     c = ''
+    if len(data) % 2 != 0:
+            return data
+    
+    
     for i in range(0, len(data), 2):
         c = c + '1' if data[i] == data[i+1] else c + '0'
         
@@ -42,9 +43,18 @@ def checksum(data):
   
 def partchecksum(data):
     c = ''
-    for i in range(0, len(data), 2):
-        c = c + '1' if data[i] == data[i+1] else c + '0'
-        
+  
+    ctr = data
+    while len(ctr) > 1:
+        t = ctr[0:2]
+        ctr = ctr[2:]
+        c = c + '1' if t[0] == t[1] else c + '0'
+    c = c + ctr
+    
+    # for i in range(0, len(data), 2):
+    #     if i + 1 > len(data):
+    #     c = c + '1' if data[i] == data[i+1] else c + '0'
+    return c
     if len(partchecksum(c)) % 2 != 0:
         return c
     
@@ -52,14 +62,11 @@ def partchecksum(data):
     
        
 
-d = create(start, 272)
+d = create(start, 272, len(start))
 c = checksum(d)
-
 print("Part 1 : " , c, c=='10011010010010010')
 
+d = create(start, 35651584, len(start))
+c = checksum(d)
+print("Part 2 : " , c)
 
-
-
-# d = create(start, 35651584)
-# c = checksum(d)
-# print("Part 21 : " , c)
