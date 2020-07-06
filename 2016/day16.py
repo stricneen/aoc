9a94f7adba6c8c91c00e99c2f01aa1a4a4e8be90
Paul Brown <paul.brown@bjss.com>
2020-07-06 10:36:57 +0100

    Day 16 play

--- 2016/day16.py
+++ 2016/day16.py
@@ -8,10 +8,6 @@ start = '00111101111101000'
 
 def step(data):
     b = ''
-    # while len(data) % 2 == 0:
-    #     print(len(data))
-    # data = partchecksum(data)
-    
     for c in data:
         d = '0' if c == '1' else '1'
         b = d + b
@@ -19,9 +15,10 @@ def step(data):
 
 def create(data, sizereq, current):
     s = step(data)
-    
+    # print(data)
+    # print(s)
+    # print()
     current = (current * 2) + 1    
-    print(current, sizereq)
     if current >= sizereq:
         return s[0:sizereq]
     else:
@@ -32,7 +29,6 @@ def checksum(data):
     if len(data) % 2 != 0:
             return data
     
-    
     for i in range(0, len(data), 2):
         c = c + '1' if data[i] == data[i+1] else c + '0'
         
@@ -40,7 +36,32 @@ def checksum(data):
             return checksum(c)
     return c
 
-  
+def rev(inp):
+    r = ''
+    for c in inp:
+        r = c + r
+    return r
+
+# def seq(fwd):
+#     bck = ''
+#     for c in rev(fwd):
+#         bck =  bck + '0' if c == '1' else bck + '1'
+#     while True:
+#         for c in fwd:
+#             yield c
+#         yield 0
+#         for c in bck:
+#             yield c
+#         yield 1
+    
+def inter(folds):
+    s = '0'
+    for _ in range(folds):
+        s = step(s)
+    return s
+    
+    
+
 def partchecksum(data):
     c = ''
   
@@ -64,9 +85,51 @@ def partchecksum(data):
 
 d = create(start, 272, len(start))
 c = checksum(d)
-print("Part 1 : " , c, c=='10011010010010010')
+print("Part 1 : ", c, c == '10011010010010010')
 
-d = create(start, 35651584, len(start))
-c = checksum(d)
-print("Part 2 : " , c)
+
+#create(start, 80, 3)
+
+def times(start,req):
+    c = start
+    t = 0
+    while c <= req:
+        t += 1
+        c = (c*2) + 1
+    return t
+    
+t1 = times(len(start), 272)
+t2 = times(len(start), 27356515842)
+
+print(t1)
+print(t2)
+
+
+
+f1 = inter(t1)
+f2 = inter(t2)
+
+print(f1)
+print(f2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print()
+# gen = seq(start)
+# for c in range(272):
+#     print (next(gen), end='')
+
+# d = create(start, 35651584, len(start))
+# c = checksum(d)
+# print("Part 2 : " , c)
 
