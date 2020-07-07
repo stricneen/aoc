eb38844ce6e98df2c4dfb84d96b1d2ec545e72c9
Paul Brown <paul.brown@bjss.com>
2020-07-07 10:30:49 +0100

    Tidy

--- 2016/day19.py
+++ 2016/day19.py
@@ -9,25 +9,3 @@ def calc(num):
 
 
 print("Part 1", calc(inp))
-
-
-
-
-
-# t = []
-# for i in range(1, inp+1):
-#     t.append(i)
-    
-# elf = 1
-# while len(t) > 1:
-#     c = t.index(elf)
-#     n = t[(c+1)%len(t)]
-#     elf = t[(c+2)%len(t)]
-#     t.remove(n)
-#     print(len(t))
-    
-# print(t)
-
-# 1 2 3 4 5
-# 1   3 4 5
-# 1   3   5
