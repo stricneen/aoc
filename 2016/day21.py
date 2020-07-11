56e8c1d082e492548168df99491a70c0d1cde466
Paul Brown <paul.brown@bjss.com>
2020-07-11 16:44:07 +0100

    Day 21-1

--- 2016/day21.py
+++ 2016/day21.py
@@ -3,21 +3,23 @@ l = f.read()
 lines = l.split('\n')
 
 password = 'abcdefgh'
+# password = 'abcde'
 chars = [char for char in password]  
 
 for l in lines:
     if l == '@':
         break
-    print(l)
+    # print(l)
     
-    l = 'rotate right 1 steps'
-    
+    # l = 'rotate right 1 steps'
+    f = False
     p = l.split(' ')
     if l.startswith("swap position"): # swap position 2 with position 3
         x, y = int(p[2]), int(p[5])
         t = chars[x]
         chars[x] = chars[y]
         chars[y] = t
+        f=True
     
     if l.startswith("swap letter"): # swap letter b with letter f
         x, y = p[2], p[5]
@@ -25,17 +27,51 @@ for l in lines:
         yi = chars.index(y)
         chars[xi] = y
         chars[yi] = x
-        
+        f=True
+   
     if l.startswith("rotate left"):
         x = int(p[2])
         s = chars[:x]
         e = chars[x:]
         chars = e + s
-        
+        f=True
+
     if l.startswith("rotate right"):
         x = int(p[2])
         s = chars[-x:]
-        e = chars[0:len(chars)-1]
+        e = chars[0:len(chars) - len(s)]
         chars = s + e
+        f=True
         
+    if l.startswith("rotate based"): # rotate based on position of letter e
+        x = chars.index(p[6]) + 1
+        if x >= 5:
+            x+=1
+        for _ in range(x):
+            s = chars[:-1]
+            e = [chars[-1]]
+            chars = e + s
+        f=True
+
+    if l.startswith("move position"): # move position 6 to position 0
+        i = int(p[2])
+        c = chars[i]
+        chars.remove(c)
+        chars.insert(int(p[5]), c)
+        f=True
+
+    if l.startswith("reverse positions"): # reverse positions 2 through 6
+        x,y = int(p[2]), int(p[4])
+        copy = chars.copy()
+        for i in range(x ,y+1):
+            chars[i] = copy[x+y-i]
+        f=True
     
+    if not f:
+        print(l)
+    
+    print(l)
+    print(''.join(chars))
+    print()
+    
+    # edgbcfah
