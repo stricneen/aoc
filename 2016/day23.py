1e94771d44d28329f1571fa7f35d151693fb2fb6
Paul Brown <paul.brown@bjss.com>
2020-07-14 13:20:51 +0100

    Day 23-1

--- 2016/day23.py
+++ 2016/day23.py
@@ -1,8 +1,8 @@
-f = open('data/day12.txt')
+f = open('data/day23.txt')
 l = f.read()
 lines = l.split('\n')
 
-a,b,c,d = 0,0,0,0
+a,b,c,d = 7,0,0,0
 prog = []
 counter = 0
 for l in lines:
@@ -29,15 +29,24 @@ while counter < len(prog):
     
     
     if inst[0] == 'jnz':
-        if inst[1] == 'a' and a != 0:
+        if inst[2] == 'a' and a != 0:
+            inc = a
+        elif inst[2] == 'b' and b != 0:
+            inc = b
+        elif inst[2] == 'c' and c != 0:
+            inc = c
+        elif inst[2] == 'd' and d != 0:
+            inc = d
+        
+        elif inst[1] == 'a' and a != 0:
             inc = int(inst[2])
-        if inst[1] == 'b' and b != 0:
+        elif inst[1] == 'b' and b != 0:
             inc = int(inst[2])
-        if inst[1] == 'c' and c != 0:
+        elif inst[1] == 'c' and c != 0:
             inc = int(inst[2])
-        if inst[1] == 'd' and d != 0:
+        elif inst[1] == 'd' and d != 0:
             inc = int(inst[2])
-        if inst[1].isnumeric() and int(inst[1]) > 0:
+        elif inst[1].isnumeric() and int(inst[1]) > 0:
             inc = int(inst[2])
             
     
@@ -70,6 +79,27 @@ while counter < len(prog):
             c -= 1
         if inst[1] == 'd':
             d -= 1
+            
+    if inst[0] == 'tgl':
+        
+        # get inst
+        reg = decide(a,b,c,d,inst[1])
+        if counter+reg < len(prog):
+            tglinst = prog[counter + reg].split(' ')
+            
+            if len(tglinst) == 2:
+                if tglinst[0] == 'inc':
+                    prog[counter + reg] = 'dec ' + tglinst[1]
+                else:
+                    prog[counter + reg] = 'inc ' + tglinst[1]
+            
+            if len(tglinst) == 3:
+                if tglinst[0] == 'jnz':
+                    prog[counter + reg] = 'cpy ' + tglinst[1] + ' ' + tglinst[2]
+                else:
+                    prog[counter + reg] = 'jnz ' + tglinst[1] + ' ' + tglinst[2]
+                    
+                
             
     counter += inc
     
