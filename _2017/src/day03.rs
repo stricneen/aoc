9cb7f494b8b6c6c2ffc304efe2e17ff1e0b1ee53
Paul Brown <paul.brown@bjss.com>
2020-07-16 22:06:46 +0100

    Refac

--- _2017/src/day03.rs
+++ _2017/src/day03.rs
@@ -1,22 +1,56 @@
 
 pub fn day3() {
     
-    let input = 361527;
+    let input: i32 = 361527;
 
+    let (d,y) = dist(input);
+
+    println!("Part 1 : {}\t{}", d, y);
     // let v: Vec<i32> = Vec::new();
     // 1     9    17   25  33  
 
-    let mut e = vec![1, 2];
-    for i in 1..10 {
-        e.push(  e.last().unwrap() + (i * 8) + 1 );
-    }
+    // let mut e = vec![1, 2];
+    // for i in 1..10000 {
+
+    //     let east =  e.last().unwrap() + (i * 8) + 1;
+
+    //     e.push(east);
+    //     // println!("{}\t {}", i + 1,  east);
+    //     if east > input { //why?
+    //         // println!("{} : {}", e.last().unwrap() , &input);
+    //         break;
+    //     }
+    // }
 
 // 1   2   11   28    53   86
 
 //     1    9   17    25
 
-    pv(e);
+   // pv(e);
 }
+
+fn dist(to: i32) -> (i32, i32) {
+    let mut e = vec![1, 2];
+    let mut d = 0;
+    let mut r = 0;
+    for i in 1..10000 {
+        let east =  e.last().unwrap() + (i * 8) + 1;
+        
+        // println!("{}\t {}", i + 1,  east);
+        if east > to { //why?
+            // println!("{} : {}", e.last().unwrap() , &input);
+            r = *e.last().unwrap();
+            d = i;
+            break;
+        }
+        e.push(east);
+    }
+
+    return (r,d);
+    // let east =  e.last().unwrap() + (i * 8) + 1;
+    // return 34;
+}
+
 
 fn ps(t: &str) {
     println!("{}", t)
