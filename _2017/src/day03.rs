6b66262bdd8232405901821a770adb333483b477
Paul Brown <paul.brown@bjss.com>
2020-07-16 19:49:42 +0100

    East

--- _2017/src/day03.rs
+++ _2017/src/day03.rs
@@ -1,6 +1,30 @@
 
 pub fn day3() {
+    
     let input = 361527;
 
+    // let v: Vec<i32> = Vec::new();
+    // 1     9    17   25  33  
 
+    let mut e = vec![1, 2];
+    for i in 1..10 {
+        e.push(  e.last().unwrap() + (i * 8) + 1 );
+    }
+
+// 1   2   11   28    53   86
+
+//     1    9   17    25
+
+    pv(e);
+}
+
+fn ps(t: &str) {
+    println!("{}", t)
+}
+
+fn pv(t: Vec<i32>) {
+    for n in t {
+        println!("{}", n);
+    }
+    println!();
 }
