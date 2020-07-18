57dfcf32f4fda74856104e4c2fd26ec81a0c1f26
Paul Brown <paul.brown@bjss.com>
2020-07-18 15:04:36 +0100

    4-1

--- _2017/src/day04.rs
+++ _2017/src/day04.rs
@@ -1,13 +1,30 @@
-// use itertools::Itertools; 
-
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 pub fn day4() {
     
-    let filename = "data/day04.txt";
-    for x in common::common::FileLines::new(filename.to_string()) {
-
-        println!("=> {}", x);
+    fn hashset(data: Vec<&str>) -> HashSet<&str> {
+        HashSet::from_iter(data.iter().cloned())
     }
+
+    let mut total1 = 0;
+    let filename = "data/day04.txt";
+    for line in common::common::FileLines::new(filename.to_string()) {
+
+        //println!("=> {}", line);
+
+        let words:Vec<&str> = line.split_whitespace().collect();
+
+        let length = words.len();
+        let hash = hashset(words);
+
+        if length == hash.len() {
+            total1 += 1;
+        }
+        
+        
+    }
+    println!("Part 1 :  {}", total1);
 }
 
 
