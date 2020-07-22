8a0c0766598642a4140909740d6b7466bd8d22c3
Paul Brown <paul.brown@bjss.com>
2020-07-22 18:10:41 +0100

    Parsing

--- _2017/src/day07.rs
+++ _2017/src/day07.rs
@@ -1,26 +1,33 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
+
 
 pub fn day7() {
-    let filename = "data/day07.txt";
+    let filename = "data/day.txt";
 
     let mut under = HashSet::<String>::new();
     let mut over = HashSet::<String>::new();
 
+    let mut weights = HashMap::new();
+
     for line in common::common::FileLines::new(filename.to_string()) {
-        // println!("{}", line);
-
         let words: Vec::<&str> = line.split_whitespace().collect();
-            
+
         under.insert(words[0].to_string());
-
+        let mut o = Vec::<String>::new();
         if words.len() > 3 {
-
             for w in words.iter().skip(3) {
+                o.push(w.to_string().replace(",", ""));
                 over.insert(w.to_string().replace(",", ""));    
             }
-
         }
 
+
+        weights.insert(words[0].to_string(), (words[1].replace("(","").replace(")","").parse::<i32>().unwrap(), o));
+    }
+
+    for (k,v) in &weights {
+        println!("{} : {:?}", k,v );
     }
 
     for w in under.iter() {
@@ -29,7 +36,7 @@ pub fn day7() {
         }
     }
 
+
     // println!("{:?}", under);
     // println!("{:?}", over);
-
 }
