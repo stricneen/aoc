9e082508e21bc951746741d899f139920884b33d
Paul Brown <paul.brown@bjss.com>
2020-07-22 07:04:09 +0100

    6-1

--- _2017/src/day06.rs
+++ _2017/src/day06.rs
@@ -3,15 +3,14 @@ use std::collections::HashSet;
 pub fn day6() {
 
     let input = "2	8	8	5	4	2	3	1	5	5	1	2	15	13	5	14";
-
-    let mut banks = common::common::split_to_int(input);
-    // let mut banks = vec![0, 2, 7 ,0]; // test
-
+    let banks = common::common::split_to_int(input);
+    //let mut banks = vec![0, 2, 7 ,0]; // test
     let r = step(banks);
+    println!("Part 1 : {}", r.1);
 
-    println!("Part 1 : {}", r.1)
-    
- 
+    let r2 = step(r.0);
+    println!("Part 2 : {}", r2.1);
+
 }
 
 fn step(mut banks: Vec<i32>) -> (Vec<i32>, i32) {
@@ -39,7 +38,7 @@ fn step(mut banks: Vec<i32>) -> (Vec<i32>, i32) {
         }
 
         state = banks.iter().map(|x| format!("-{}", x)).collect::<String>();
-        println!("{}", state);
+        // println!("{}", state);
 
     }
 
