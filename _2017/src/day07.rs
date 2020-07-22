a57682b34fb0af3e05c3b2f35dd3eef106ff59db
Paul Brown <paul.brown@bjss.com>
2020-07-22 16:45:00 +0100

    7-1

--- _2017/src/day07.rs
+++ _2017/src/day07.rs
@@ -1,7 +1,35 @@
+use std::collections::HashSet;
 
 pub fn day7() {
     let filename = "data/day07.txt";
+
+    let mut under = HashSet::<String>::new();
+    let mut over = HashSet::<String>::new();
+
     for line in common::common::FileLines::new(filename.to_string()) {
-        println!("{}", line);
+        // println!("{}", line);
+
+        let words: Vec::<&str> = line.split_whitespace().collect();
+            
+        under.insert(words[0].to_string());
+
+        if words.len() > 3 {
+
+            for w in words.iter().skip(3) {
+                over.insert(w.to_string().replace(",", ""));    
+            }
+
+        }
+
     }
+
+    for w in under.iter() {
+        if over.contains(w) == false {
+            println!("Part 1 : {}", w);
+        }
+    }
+
+    // println!("{:?}", under);
+    // println!("{:?}", over);
+
 }
