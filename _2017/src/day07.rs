e8a7657cef865347495176967e581258e5fd21f5
Paul Brown <paul.brown@bjss.com>
2020-08-03 21:57:53 +0100

    Correct weights

--- _2017/src/day07.rs
+++ _2017/src/day07.rs
@@ -21,8 +21,6 @@ pub fn day7() {
                 over.insert(w.to_string().replace(",", ""));    
             }
         }
-
-
         weights.insert(words[0].to_string(), (words[1].replace("(","").replace(")","").parse::<i32>().unwrap(), o));
     }
 
@@ -36,23 +34,40 @@ pub fn day7() {
         }
     }
 
-
     let mut holds = HashMap::new();
 
     for w in weights.keys() {
-
         let bob = calc(0, w, &weights);
         holds.insert(w, bob);
     }
-   
-     println!("Part 1 : {:?}", holds);
+
+    println!("Weights : {:?}", holds);
+
+
+    println!("padx : {:?}", holds[&"padx".to_string()])
+
+
 }
 
-fn calc (acc: i32, disc: &String, weights: &HashMap<String, (i32, Vec<std::string::String>)>) -> i32 {
-    // let mut total = 0;
-    for over in &weights[disc].1 {
-        return calc(acc + weights[disc].0, &over, &weights);
+fn calc (mut acc: i32, disc: &String, weights: &HashMap<String, (i32, Vec<std::string::String>)>) -> i32 {
+     //let mut total = 0;
+   // acc += weights[disc].0;
+    
+    if &weights[disc].1.len() > &0 {
+        acc += weights[disc].0;
+        for over in &weights[disc].1 {
+            acc += calc(acc, &over, &weights);
+        }    
     }
-    return acc + weights[disc].0;
-}
+    else {
+        acc = weights[disc].0;
+    }
 
+    // for over in &weights[disc].1 {
+    //     acc += calc(weights[disc].0, &over, &weights);
+    // }
+
+    return acc;
+
+
+}
