bd15dbdd324f34d95268323e87d18c14c113292f
Paul Brown <paul.brown@bjss.com>
2020-08-18 21:34:29 +0100

    11-2

--- _2017/src/day11.rs
+++ _2017/src/day11.rs
@@ -7,38 +7,36 @@ pub fn day11() {
         let words: Vec::<&str> = line.split(",").collect();
         // println!("{:?}", words);
 
-        assert_eq!(dist(travel(&"ne,ne,ne".split(",").collect())), 3);
-        assert_eq!(dist(travel(&"ne,ne,sw,sw".split(",").collect())), 0);
-        assert_eq!(dist(travel(&"ne,ne,s,s".split(",").collect())), 2);
-        assert_eq!(dist(travel(&"se,sw,se,sw,sw".split(",").collect())), 3);
-        
-        
-        // ne,ne,ne is 3 steps away.
-        // ne,ne,sw,sw is 0 steps away (back where you started).
-        // ne,ne,s,s is 2 steps away (se,se).
-        // se,sw,se,sw,sw is 3 steps away (s,s,sw).
+        assert_eq!(dist(travel(&"ne,ne,ne".split(",").collect()).0), 3);
+        assert_eq!(dist(travel(&"ne,ne,sw,sw".split(",").collect()).0), 0);
+        assert_eq!(dist(travel(&"ne,ne,s,s".split(",").collect()).0), 2);
+        assert_eq!(dist(travel(&"se,sw,se,sw,sw".split(",").collect()).0), 3);
 
-        println!("Part 1 : {}", dist(travel(&words))); // pencil
-
-        println!("{:?} ", travel(&words)); // th 913
-
-
+        println!("Part 1 : {}", dist(travel(&words).0));
+        println!("Part 2 : {}", travel(&words).1);
     }
 }
 
-fn travel(words: &Vec::<&str>) -> (i32, i32) {
+fn travel(words: &Vec::<&str>) -> ((i32, i32), u32) {
+    let mut mx = 0;
     let t = words.iter().fold((0,0), |acc, &x| {
-        match x {
-            "n" => (acc.0 + 1 ,acc.1),
-            "s" => (acc.0 - 1 ,acc.1),
-            "nw" => (acc.0 + 1 ,acc.1 - 1),
-            "se" => (acc.0 - 1 ,acc.1 + 1),
-            "ne" => (acc.0 ,acc.1 + 1),
-            "sw" => (acc.0 ,acc.1 - 1),
+
+        let step = match x {
+            "n" => (acc.0 + 1, acc.1),
+            "s" => (acc.0 - 1, acc.1),
+            "nw" => (acc.0 + 1, acc.1 - 1),
+            "se" => (acc.0 - 1, acc.1 + 1),
+            "ne" => (acc.0, acc.1 + 1),
+            "sw" => (acc.0, acc.1 - 1),
             _ => panic!((0,0))
-        } 
+        };
+        let d = dist(step);
+        if d > mx {
+            mx = d;
+        }
+        return step;
     });
-    return t;
+    return (t, mx);
 }
 
 fn dist(pos: (i32, i32)) -> u32 {
@@ -53,20 +51,3 @@ fn dist(pos: (i32, i32)) -> u32 {
         }
     }
 }
-
-// fn dist2(words: Vec::<&str>) -> usize {
-//     let n = words.iter().filter(|&x| *x == "n").count();
-//     let s = words.iter().filter(|&x| *x == "s").count();
-
-//     let nw = words.iter().filter(|&x| *x == "nw").count();
-//     let se = words.iter().filter(|&x| *x == "se").count();
-
-//     let ne = words.iter().filter(|&x| *x == "ne").count();
-//     let sw = words.iter().filter(|&x| *x == "sw").count();
-//     println!("{} {}     {} {}     {} {}",n,s,nw,se,ne,sw);
-//     let p1 = ((n as i32) - (s as i32)).abs();
-//     let p2 = ((nw as i32) - (se as i32)).abs();
-//     let p3 = ((ne as i32) - (sw as i32)).abs();
-//     println!("{} {} {}", p1,p2,p3);
-//     return p1 as usize + p2 as usize + p3 as usize;
-// }
