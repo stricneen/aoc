7a064bc9421491e0e61ab5f13d0053cb91894d21
Paul Brown <paul.brown@bjss.com>
2020-08-22 15:53:00 +0100

    13-1

--- _2017/src/day13.rs
+++ _2017/src/day13.rs
@@ -1,9 +1,33 @@
+use std::collections::HashMap;
 
 pub fn day() {
     let filename = "data/day13.txt";
+    let mut firewall = HashMap::new();
     for line in common::common::FileLines::new(filename.to_string()) {
-        println!("{}", line);
+        
+        let words: Vec::<&str> = line
+            .split(':')
+            .collect();
+       
+        firewall.insert(
+            words[0].parse::<i32>().unwrap(),
+            words[1].trim().parse::<i32>().unwrap()
+        );
+    } 
+    // println!("{:?}", firewall);
 
-        let _words: Vec::<&str> = line.split_whitespace().collect();
-    }
+    let p1 = firewall.iter().fold(0, |acc, x| {
+        if x.0 <= &1 {
+            return acc
+        }
+
+        if  x.0 % ((x.1 -1 ) * 2) == 0 {
+            acc + (x.0 * x.1)
+        } else {
+            acc
+        }
+    });
+    
+
+    println!("Part 1 : {:?}", p1);
 }
