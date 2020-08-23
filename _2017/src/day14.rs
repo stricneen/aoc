39a78c3300ed41f90f4aff77e5ce10191378c000
Paul Brown <paul.brown@bjss.com>
2020-08-23 14:34:50 +0100

    14 refac

--- _2017/src/day14.rs
+++ _2017/src/day14.rs
@@ -1,27 +1,33 @@
-use crate::day10::knot_hash;
+use crate::day10;
 
 pub fn day14() {
 
     let i = "jzgqcdpd";
     let t = "flqrgnkx";
-    let input =  String::from(i);
+    let input =  String::from(t);
 
     let mut p1 = 0;
+    let mut grid = vec![];
+
     for i in 0..128 {
 
         let l = format!("{}-{}", input, i);
-        let hash = knot_hash(&l);
+        let hash = day10::knot_hash(&l);
 
-        let bin = hash.chars().fold(0, |acc,x| {
+        let bin = hash.chars().fold("".to_string(), |acc,x| {
             let hex = i32::from_str_radix(&x.to_string(), 16).unwrap();
             let bin = to_binary(hex, 4);
-            return acc + bin.matches("1").count();
+            return format!("{}{}", acc, bin);
         });        
-        p1 += bin;
 
+        p1 += bin.matches("1").count();
+        grid.push(bin);
     }
-
     println!("Part 1 : {}", p1);
+
+    for x in grid {
+        println!("{}", x);
+    }
 
 }
 
