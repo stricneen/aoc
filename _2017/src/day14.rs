6f19f1bc307ea3994e3610a70fcd2352c71dc0f9
Paul Brown <paul.brown@bjss.com>
2020-08-24 08:43:46 +0100

    Flood

--- _2017/src/day14.rs
+++ _2017/src/day14.rs
@@ -13,7 +13,6 @@ pub fn day14() {
         let mut j = 0;
         let l = format!("{}-{}", input, i);
         let hash = day10::knot_hash(&l);
-
         let bin = hash.chars().fold("".to_string(), |acc,x| {
             let hex = i32::from_str_radix(&x.to_string(), 16).unwrap();
             let bin = to_binary(hex, 4);
@@ -22,74 +21,68 @@ pub fn day14() {
             }
             j += 4;
             return format!("{}{}", acc, bin);
-        });        
-        
+        });  
         p1 += bin.matches("1").count();
     }
     println!("Part 1 : {}", p1);
-
-    // for x in &grid {
-    //     println!("{}", x);
-    // }
     
     print_grid(&grid);
 
-//     let p2 = count_clusters(&grid);
-//     // assert_eq!(1242, p2);
-//     println!("Part 2 : {}", p2);
+    let p2 = count_clusters(&mut grid);
+    //assert_eq!(1242, p2);
+    println!("Part 2 : {}", p2);
 
 //     let x = flood(&grid, 0, 0);
 //     print_grid(&grid);
 }
 
-fn count_clusters(grid: &Vec<String>) -> i32 {
+fn count_clusters(grid: &mut [[u8; 128]; 128]) -> i32 {
 
-    for row in grid {
-        let x = row.find('x');
+    for i in 0..grid.len() {
 
-        if x.is_some() {
+        let row = grid[i];
+        let j = row.iter().position(|x| x == &1);
 
+        if j.is_some() {
+            flood(grid, i ,j.unwrap());
         }
-        
-        print!("{:?}", x);
-        
     }
+    print_grid(&grid);
+
     return 0;
 }
 
-// fn flood(grid: &Vec<String>, x: usize, y: usize) {
-//     let mut cells = vec![(x,y)];
-//     let mut size: usize = 0;
+fn flood(grid: &mut[[u8; 128]; 128], x: usize, y: usize) {
+    let mut cells = vec![(x,y)];
+    let mut size: usize = 0;
 
-//     while cells.len() !=  size {
-//         size = cells.len();
+    while cells.len() !=  size {
+        size = cells.len();
         
-//         for c in &mut cells {
+        let mut adj_cell = vec![];
+        for c in &cells {
+            adj_cell.append(&mut get_adj(&grid, c.0, c.1));
+          
+        }
+        cells.append(&mut adj_cell);
+        cells.sort();
+        cells.dedup();
+    }
 
-//             let adj_cell = get_adj(&grid, c.0, c.1);
+    for f in cells {
+        grid[f.0][f.1] = 2;
+    }
+}
 
-//             for ac in adj_cell{
-//                // if cells.contains(adj_cell) {
-//                 //&mut cells.push(ac);
-//                 //}
-//             }
-//         }
-//     }
-// }
+fn get_adj(grid: &[[u8; 128]; 128], x: usize, y: usize) -> Vec<(usize, usize)>{
+    let mut cells = vec![];
+    if x > 0 && grid[x-1][y] == 1 { cells.push((x-1,y)) }
+    if y > 0 && grid[x][y-1] == 1 { cells.push((x,y-1)) }
+    if x < grid[0].len() - 1 && grid[x+1][y] == 1 { cells.push((x+1,y)) }
+    if y < grid.len() - 1 && grid[x][y+1] == 1 { cells.push((x,y+1)) }
+    return cells;
+}
 
-// fn get_adj(grid: &Vec<String>, x: usize, y: usize) -> Vec<(usize,usize)>{
-//     let mut cells = vec![];
-//     if x >= 0 && read_cell(&grid, x-1, y) == '1' { cells.push((x-1,y)) }
-//     if y >= 0 && read_cell(&grid, x, y-1) == '1' { cells.push((x,y-1)) }
-//     if x < grid[0].len() && read_cell(&grid, x+1, y) == '1' { cells.push((x+1,y)) }
-//     if y < grid.len() && read_cell(&grid, x, y+1) == '1' { cells.push((x,y+1)) }
-    
-//     return cells;
-// }
-
-// fn read_cell(grid: &Vec<String>, x: usize, y: usize) -> char {
-//      return grid[x].chars().nth(y).unwrap();
-// }
 
 // fn write_cell(grid: &mut Vec<String>, x: usize, y: usize, v: char) {
 //     let rs = grid[x].clone();
@@ -111,12 +104,15 @@ fn to_binary(i: i32, l: usize) -> String{
 }
 
 fn print_grid(grid: &[[u8; 128]; 128]) {
+    println!();
     for row in grid.iter() {
         for cell in row.iter() {
             if *cell == 0 {
                 print!("\x1b[0;30m{}\x1b[0m", cell);
-            } else {
+            } else if *cell == 1 {
                 print!("\x1b[0;33m{}\x1b[0m", cell);
+            } else {
+                print!("\x1b[0;34m{}\x1b[0m", cell);
             }
         }
         println!();
