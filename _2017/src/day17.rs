2d6a04be614ab88ac0b3640211860e810a1eeed6
Paul Brown <paul.brown@bjss.com>
2020-09-05 12:17:39 +0100

    17-2

--- _2017/src/day17.rs
+++ _2017/src/day17.rs
@@ -10,5 +10,19 @@ pub fn day17() {
 
     let pos_2017 = spin_lock.iter().position(|&r| r == 2017).unwrap();
     println!("Part 1 : {:?}", spin_lock[pos_2017 + 1]);
+
+
+    // println!("{:?}", spin_lock);
+    let mut spin_lock_len = 1;
+    let mut pos = 0;
+    let mut pos_2 = 0;
+    for i in 1..=50000000 {
+        pos = (pos + input_step) % spin_lock_len + 1;
+        spin_lock_len += 1;
+        if pos == 1 {
+            pos_2 = i;
+        }
+    }
+    println!("Part 2 : {:?}", pos_2);
 }
 
