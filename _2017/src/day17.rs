b13e49102e608c33d1f03bb452c455a7c9d311bf
Paul Brown <paul.brown@bjss.com>
2020-09-05 12:09:12 +0100

    17-1

--- _2017/src/day17.rs
+++ _2017/src/day17.rs
@@ -1,7 +1,14 @@
 pub fn day17() {
-    let input = 337;
+    let input_step = 337;
 
+    let mut spin_lock = vec![0];
+    let mut pos = 0;
+    for i in 1..=2017 {
+        pos = (pos + input_step) % spin_lock.len() + 1;
+        spin_lock.insert(pos, i);
+    }
 
-    println!("Input : {}", input);
-
+    let pos_2017 = spin_lock.iter().position(|&r| r == 2017).unwrap();
+    println!("Part 1 : {:?}", spin_lock[pos_2017 + 1]);
 }
+
