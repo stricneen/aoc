99e4a994f12a5b909d91f448c69f5dbe1953a1ee
Paul Brown <paul.brown@bjss.com>
2020-09-05 09:51:56 +0100

    faster

--- _2017/src/day16.rs
+++ _2017/src/day16.rs
@@ -4,69 +4,67 @@ pub fn day16() {
     for line in common::common::FileLines::new(filename.to_string()) {
         
         let words: Vec::<&str> = line.split(',').collect();
-        let mut row = String::from("abcdefghijklmnop");      // let r = row..iter().skip(4).take();
-        //println!("{:?}", words);
+        let o_row = String::from("abcdefghijklmnop");      
+        let mut row = String::from("abcdefghijklmnop");      
+        // 33 dmokfibagnjplhec
+        // 34 nligokdebfcmjhpa
+        // 35 ljfgibhcmpdaenko
+       
+    for i in 0..1_000_000_000  {
+       
+        for inst in &words {
+            //println!("{}", inst);
+            // s1, a spin of size 1: eabcd.
+            // ,x3/4 swapping the last two programs: eabdc.
+            // pe/b, swapping programs e and b: baedc.
 
-        let len = row.len() as i32;
-        let sp_words = words.iter().fold((0, vec![]), |mut acc, x| {
-            if x.starts_with('s') {
-                let spl = x.chars().skip(1).collect::<String>().parse::<i32>().unwrap();
-                // acc.0 += spl;
-                // acc.1.push("s0")
-                // acc.1.push("s0".to_string());
-                //println!("{:?} {}",acc.0 ,x);
-                return (acc.0 + spl, acc.1);
-            } else if x.starts_with('x') {
-                 let a1 = x.chars().skip(1).take_while(|x| x != &'/').collect::<String>().parse::<i32>().unwrap();
-                 let b1 = x.chars().skip_while(|x| x != &'/').skip(1).collect::<String>().parse::<i32>().unwrap();
-                 let new_instruction = format!("x{}/{}", (a1 - (acc.0 % len) + len) % len, (b1 - (acc.0 % len) + len) % len);
-                 acc.1.push(new_instruction);
-                return acc;
-            } else {
-                //acc.1.push(x);
-                acc.1.push(x.to_string());
-                return acc;
+
+            if inst.starts_with('s') {
+                let spl = inst.chars().skip(1).collect::<String>().parse::<usize>().unwrap();
+                let s = row.chars().skip(16-spl).collect::<String>();
+                let t = row.chars().take(16-spl).collect::<String>();
+                row = s + &t;
             }
-        });
 
-        let mut instructions = sp_words.1;
-        instructions.push(format!("s{}", sp_words.0 % row.len() as i32));
+            if inst.starts_with('x') {
+                let a1 = inst.chars().skip(1).take_while(|x| x != &'/').collect::<String>().parse::<usize>().unwrap();
+                let b1 = inst.chars().skip_while(|x| x != &'/').skip(1).collect::<String>().parse::<usize>().unwrap();
+                            
+                let a = row.chars().nth(a1).unwrap();
+                let b = row.chars().nth(b1).unwrap();
 
+                unsafe {
+                    let bytes = row.as_bytes_mut();
+                    bytes[a1] = b as u8;
+                    bytes[b1] = a as u8;
+                }
+                // row = row.replace(&a, "!");
+                // row = row.replace(&b, &a);
+                // row = row.replace("!", &b);
+            }
 
-    for inst in instructions {
-        //println!("{}", inst);
-        // s1, a spin of size 1: eabcd.
-        // ,x3/4 swapping the last two programs: eabdc.
-        // pe/b, swapping programs e and b: baedc.
-        if inst.starts_with('s') {
-            let spl = inst.chars().skip(1).collect::<String>().parse::<usize>().unwrap();
-            let s = row.chars().skip(16-spl).collect::<String>();
-            let t = row.chars().take(16-spl).collect::<String>();
-            row = s + &t;
+            if inst.starts_with('p') {
+                let a = inst.chars().skip(1).nth(0).unwrap();
+                let b = inst.chars().skip(3).nth(0).unwrap();
+                let a1 = row.find(a).unwrap();
+                let b1 = row.find(b).unwrap();
+                unsafe {
+                    let bytes = row.as_bytes_mut();
+                    bytes[a1] = b as u8;
+                    bytes[b1] = a as u8;
+                }
+                // row = row.replace(&a, "!");
+                // row = row.replace(&b, &a);
+                // row = row.replace("!", &b);
+            }        
         }
-
-        if inst.starts_with('x') {
-            let a1 = inst.chars().skip(1).take_while(|x| x != &'/').collect::<String>().parse::<usize>().unwrap();
-            let b1 = inst.chars().skip_while(|x| x != &'/').skip(1).collect::<String>().parse::<usize>().unwrap();
-            let a = row.chars().nth(a1).unwrap().to_string();
-            let b = row.chars().nth(b1).unwrap().to_string();
-
-            row = row.replace(&a, "!");
-            row = row.replace(&b, &a);
-            row = row.replace("!", &b);
+      
+        println!("{} {}",i, row);
+        if row == o_row {
+            break;
         }
-
-        if inst.starts_with('p') {
-            let a = inst.chars().skip(1).take(1).collect::<String>();
-            let b = inst.chars().skip(3).take(1).collect::<String>();
-            
-            row = row.replace(&a, "!");
-            row = row.replace(&b, &a);
-            row = row.replace("!", &b);
-        }        
     }
-    
-
+    //println!("Insts : {}", instructions.len());
     println!("Part 1 : {}", row);
     assert_eq!("hmefajngplkidocb", row);
         // x3/15
@@ -77,9 +75,19 @@ pub fn day16() {
         // s2
         // x8/10
 
+
+    // x  8 / 7
+    // x  6 / 5
+    // p  j / h
+    // x  10 / 3
+    // x  2  / 15
+    // x  14 / 1
+    // p  b / l
+    // x  3 / 4
         
         
         
         
     }
 }
+
