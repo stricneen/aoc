92c33098fe746e31c944b0e34441c4fcbd4cc5ea
Paul Brown <paul.brown@bjss.com>
2020-09-06 22:53:11 +0100

    19 setup

--- _2017/src/day19.rs
+++ _2017/src/day19.rs
@@ -1,9 +1,18 @@
 
 pub fn day19() {
     let filename = "data/day.txt";
+    let mut arr = vec![];
+    
     for line in common::common::FileLines::new(filename.to_string()) {
-        println!("{}", line);
-
-        let words: Vec::<&str> = line.split_whitespace().collect();
+        //println!("{}", line);
+        let r = line.chars().collect::<Vec<char>>();
+        arr.push(r);
     }
+    // println!("{:?}", arr);
+
+    // Get entrance
+    let pos = (arr[0].iter().position(|&x| x == '|').unwrap(), 0);
+    let dir = 's';
+    
+    println!("{:?}", pos);
 }
