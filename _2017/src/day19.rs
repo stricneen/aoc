bb8ce66b261024110aee355a72698c0c1e720ec1
Paul Brown <paul.brown@bjss.com>
2020-09-07 19:33:38 +0100

    19-2

--- _2017/src/day19.rs
+++ _2017/src/day19.rs
@@ -1,6 +1,6 @@
 
 pub fn day19() {
-    let filename = "data/day.txt";
+    let filename = "data/day19.txt";
     let mut arr = vec![];
     
     for line in common::common::FileLines::new(filename.to_string()) {
@@ -10,9 +10,59 @@ pub fn day19() {
     }
     // println!("{:?}", arr);
 
+    let value_at = |x: (usize, usize)| -> char {
+        return arr[x.1][x.0];
+    };
+
     // Get entrance
-    let pos = (arr[0].iter().position(|&x| x == '|').unwrap(), 0);
-    let dir = 's';
-    
-    println!("{:?}", pos);
+    let mut pos = (arr[0].iter().position(|&x| x == '|').unwrap(), 0);
+    let mut dir = 's';
+    let mut steps = 1;
+    loop {
+
+        pos = match dir {
+            'n' => (pos.0, pos.1 - 1),
+            'e' => (pos.0 + 1, pos.1),
+            's' => (pos.0, pos.1 + 1),
+            'w' => (pos.0 - 1, pos.1),
+            _ => pos
+        };
+
+        let x = value_at(pos);
+
+        if x.is_alphabetic() {
+            print!("{}", x);
+        }
+
+        if x == '+'  {
+            if dir == 'n' || dir == 's' {
+                let e = value_at((pos.0 + 1, pos.1));
+                //let w = value_at((pos.0 - 1, pos.1));
+                if e != ' ' {
+                    dir = 'e'
+                } else {
+                    dir = 'w'
+                }
+            } else if dir == 'e' || dir == 'w' {
+                let n = value_at((pos.0, pos.1 - 1));
+                //let w = value_at((pos.0, pos.1 + 1));
+                if n != ' ' {
+                    dir = 'n'
+                } else {
+                    dir = 's'
+                }
+            }
+        }
+
+        steps +=1;
+
+        if x == 'Y' || x == 'F'{
+            break;
+        }
+
+
+    }
+
+    println!();
+    println!("Part 2 : {}", steps)
 }
