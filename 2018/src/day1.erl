38bdc0885650f13c7c9990a1e6d75d72440bf26a
Paul Brown <paul.brown@bjss.com>
2020-09-17 20:40:15 +0100

    Files

--- 2018/src/day1.erl
+++ 2018/src/day1.erl
@@ -1,19 +1,25 @@
 -module(day1).
--export([fac/1]).
--export([double/1]).
+-export([day2/0]).
 
-readlines(FileName) ->
-    {ok, Data} = file:read_file(FileName),
-    binary:split(Data, [<<"\n">>], [global]).
+run() ->
+    file("../data/day01.txt").
 
+day2() ->
+    Av1 = "Hello",
+    io:format("Bla ~p~n", [Av1]),
+    io:format("Hello, world ~n").
+    %file("../data/day01.txt").
 
-exe() ->
-    readLines("../data/day1.txt"),
+file(FName) ->  % a variable must start by an Upper case character, otherwise it is an atom
+    {ok,IoDevice} = file:open(FName, [read]), % file:open/2 returns the tuple {ok,IoDevice} if it succeeds.
+                                              % IoDevice is the file descriptor you will use for further accesses
+    string:tokens(io:get_line(IoDevice,""), ". "). % you must use the file descriptor to read a new line, get_line
+                                               % is expecting 2 arguments, the second one is a prompt, not used here
+                                               % this code will split the first line of the file FName using
+                                               % the dot and the white space as separators. It will then returns
+                                               % the results letting the file open, but with the file descriptor
+                                               % lost! so no chance to continue to read the lines like this.
 
-
-
-double(Num) -> Num * 2.
-
-fac(0) -> 1;
-
-fac(N) -> N * fac(N-1).
+% readfile(FileName) ->
+%     {ok, Binary} = file:read_file(FileName),
+%     Lines = string:tokens(erlang:binary_to_list(Binary), "\n").
