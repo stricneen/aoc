cfb474d23617f47da329c6d50bb9492d4a5dc3cf
Paul Brown <paul.brown@bjss.com>
2020-09-17 21:00:24 +0100

    Printing file

--- 2018/src/day1.erl
+++ 2018/src/day1.erl
@@ -1,19 +1,20 @@
 -module(day1).
 -export([day2/0]).
 
-run() ->
-    file("../data/day01.txt").
+% run() ->
+%     file("../data/day01.txt").
 
 day2() ->
     Av1 = "Hello",
     io:format("Bla ~p~n", [Av1]),
-    io:format("Hello, world ~n").
-    %file("../data/day01.txt").
+    io:format("Day 1 ~n"),
+    file("../data/day1.txt").
 
 file(FName) ->  % a variable must start by an Upper case character, otherwise it is an atom
-    {ok,IoDevice} = file:open(FName, [read]), % file:open/2 returns the tuple {ok,IoDevice} if it succeeds.
-                                              % IoDevice is the file descriptor you will use for further accesses
-    string:tokens(io:get_line(IoDevice,""), ". "). % you must use the file descriptor to read a new line, get_line
+    % {ok,IoDevice} = file:open(FName, [read]), % file:open/2 returns the tuple {ok,IoDevice} if it succeeds.
+    {ok, Binary} = file:read_file(FName),
+    io:format(Binary).                                         % IoDevice is the file descriptor you will use for further accesses
+    %string:tokens(io:get_line(IoDevice,""), ". "). % you must use the file descriptor to read a new line, get_line
                                                % is expecting 2 arguments, the second one is a prompt, not used here
                                                % this code will split the first line of the file FName using
                                                % the dot and the white space as separators. It will then returns
