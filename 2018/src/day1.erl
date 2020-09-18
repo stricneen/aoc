884e6fa8415c5980f2ab7fd84549877855c53434
Paul Brown <paul.brown@bjss.com>
2020-09-18 08:02:37 +0100

    1-1 tidy

--- 2018/src/day1.erl
+++ 2018/src/day1.erl
@@ -1,22 +1,11 @@
 -module(day1).
--export([day2/0]).
+-export([day1/0]).
 
-% run() ->
-%     file("../data/day01.txt").
-
-day2() ->
-    % Av1 = "Hello",
-    % io:format("Bla ~p~n", [Av1]),
-    % io:format("Day 1 ~n"),
+day1() ->
     Lines = readlines("../data/day1.txt"),
-
     Lines2 = lists:map(fun(N) -> N2 = N--"\n", list_to_integer(N2) end, Lines),
-
     Total = lists:foldl(fun(A, N) -> A + N end, 0, Lines2),
-    
-
-
-    io:format(Total).
+    io:format("Part 1 : ~p~n", [Total]).
 
 readlines(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
