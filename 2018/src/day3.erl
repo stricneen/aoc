a7254427c08f2705cbcd204d3dee03604ffbad1b
Paul Brown <paul.brown@bjss.com>
2020-09-21 13:49:12 +0100

    3 parsing

--- 2018/src/day3.erl
+++ 2018/src/day3.erl
@@ -5,14 +5,27 @@
 
 run() ->
     Input = aoc:readlines("../data/day3.txt"),
-    io:format(Input),
+    %io:format(Input),
 
     % #1209 @ 525,950: 28x12
     
     Claims = lists:map(fun(X) -> 
-         #claim{x=0,y=0,w=0,h=0}
+         % io:format(X),
+         Id = string:split(X, " @ "),
+         Parts = string:split(lists:nth(2, Id), ": "),
+         Coords = string:split(lists:nth(1, Parts), ","),
+         Size = string:split(lists:nth(2, Parts), "x"),
+        % io:format(lists:nth(0, Coords)),
+         #claim{ 
+              x=list_to_integer(lists:nth(1, Coords)),
+              y=list_to_integer(lists:nth(2, Coords)),
+              w=list_to_integer(lists:nth(1, Size)),
+              h=list_to_integer(lists:nth(2, Size)) }
          end, Input),
 
-
-
-    io:format("~nPart 1 : ~p~n", [Claims]).
+% #1230 @ 28,723: 27x15
+% #1231 @ 920,956: 18x23
+% #1232 @ 23,641: 17x13
+% #1233 @ 437,811: 19x10
+   
+   io:format("~nPart 1 : ~p~n", [Claims]).
