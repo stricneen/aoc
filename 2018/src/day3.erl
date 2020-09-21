617e94f16c70cccbfea07578e6f3ba6e22ed26eb
Paul Brown <paul.brown@bjss.com>
2020-09-21 08:34:33 +0100

    records

--- 2018/src/day3.erl
+++ 2018/src/day3.erl
@@ -1,12 +1,18 @@
 -module(day3).
 -export([run/0]).
 
-
+-record(claim, {x, y, w ,h}).
 
 run() ->
     Input = aoc:readlines("../data/day3.txt"),
     io:format(Input),
 
+    % #1209 @ 525,950: 28x12
+    
+    Claims = lists:map(fun(X) -> 
+         #claim{x=0,y=0,w=0,h=0}
+         end, Input),
 
 
-    io:format("~nPart 1 : ~p~n", [0]).
+
+    io:format("~nPart 1 : ~p~n", [Claims]).
