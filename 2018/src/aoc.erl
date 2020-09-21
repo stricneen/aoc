54cb287ab8772ce5a6d929acb99df3289ce66b07
Paul Brown <paul.brown@bjss.com>
2020-09-21 18:03:03 +0100

    3-1

--- 2018/src/aoc.erl
+++ 2018/src/aoc.erl
@@ -1,5 +1,6 @@
 -module(aoc).
 -export([readlines/1]).
+-export([aggregate_list/2]).
 
 readlines(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
@@ -10,3 +11,12 @@ get_all_lines(Device, Accum) ->
         eof  -> file:close(Device), Accum;
         Line -> get_all_lines(Device, Accum ++ [string:trim(Line)])
     end.
+
+aggregate_list([H|T], Counts) ->
+    NewCounts = case dict:is_key(H, Counts) of
+        true -> dict:update(H, fun(Value) -> Value + 1 end, Counts);
+        false -> dict:store(H, 1, Counts)
+        end,
+    aggregate_list(T, NewCounts);
+
+aggregate_list([], Counts) -> Counts.
--- 2018/src/day3.erl
+++ 2018/src/day3.erl
@@ -5,27 +5,45 @@
 
 run() ->
     Input = aoc:readlines("../data/day3.txt"),
-    %io:format(Input),
+    %Input = [lists:nth(1, Input2)],
 
-    % #1209 @ 525,950: 28x12
-    
+    % Parse
     Claims = lists:map(fun(X) -> 
-         % io:format(X),
          Id = string:split(X, " @ "),
          Parts = string:split(lists:nth(2, Id), ": "),
          Coords = string:split(lists:nth(1, Parts), ","),
          Size = string:split(lists:nth(2, Parts), "x"),
-        % io:format(lists:nth(0, Coords)),
-         #claim{ 
-              x=list_to_integer(lists:nth(1, Coords)),
-              y=list_to_integer(lists:nth(2, Coords)),
-              w=list_to_integer(lists:nth(1, Size)),
-              h=list_to_integer(lists:nth(2, Size)) }
+         #claim { 
+              x = list_to_integer(lists:nth(1, Coords)),
+              y = list_to_integer(lists:nth(2, Coords)),
+              w = list_to_integer(lists:nth(1, Size)),
+              h = list_to_integer(lists:nth(2, Size)) }
          end, Input),
 
-% #1230 @ 28,723: 27x15
-% #1231 @ 920,956: 18x23
-% #1232 @ 23,641: 17x13
-% #1233 @ 437,811: 19x10
-   
-   io:format("~nPart 1 : ~p~n", [Claims]).
+     % Claim to points
+     Decode = fun(Clm) -> % returns eg [(3,4), (3,5)]
+          Rows = lists:seq(Clm#claim.x, Clm#claim.x + Clm#claim.w - 1),
+          Cols = lists:seq(Clm#claim.y, Clm#claim.y + Clm#claim.h - 1),
+
+          Res = lists:map(fun (Rs) -> 
+               Columns = lists:map(fun (Cs) ->
+                    {Rs, Cs}
+               end, Cols),
+               Columns end, Rows),
+          Res end,
+
+     Points = lists:foldl(fun(C, A) ->
+          A ++ Decode(C) end, [], Claims),
+
+     % io:format("~p", [Points]),
+
+     P = lists:foldl(fun (S,A) -> S++A end, [], Points),
+     D = aoc:aggregate_list(P, dict:new()),
+
+     P1 = dict:filter(fun(_,V)-> V > 1 end, D),
+
+     io:format("Part 1:  ~p~n ", [length(dict:fetch_keys(P1))]).
+
+
+% th 114671
+     
