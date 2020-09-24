c2b31762ecce5bcd8fa8f94941f0b6c4e7fc0d4b
Paul Brown <paul.brown@bjss.com>
2020-09-24 22:38:21 +0100

    Fold

--- 2018/src/day4.erl
+++ 2018/src/day4.erl
@@ -3,7 +3,7 @@
 
 run() ->
 
-    Input = aoc:readlines("../data/day4.txt"),
+    Input = aoc:readlines("../data/day.txt"),
     SortedInput = lists:sort(Input),
     Parsed = lists:map(fun (X) -> 
         [Time, Event] = string:lexemes(X, "]"),
@@ -12,13 +12,44 @@ run() ->
         {D, list_to_integer(S), Event}
         end, SortedInput),
 
-    Time = lists:foldl(fun({D,M,A}, Acc) -> 
-        B = case A of 
-            " G" ++ _ -> guard;
-            " f" ++ _ -> sleep;
-            " w" ++ _ -> wake
+    Time = lists:foldl(fun({_,Mins,Action}, Acc) -> 
+
+        io:format("Mins : ~p~n", [Mins]),
+
+        B = case string:trim(Action) of 
+            "G" ++ _ -> [{ string:trim(Action), [] }] ++ Acc;
+            _        -> [H|T] = Acc,
+                        { G1, T1 } = H,
+                        [{ G1, T1 ++ [Mins] }] ++ T
         end,
-        [B] ++ Acc
+
+      %  io:format((" ~p  ~p~n"),[Mins],[Action]),
+        io:format("B : ~p~n", [B]),
+
+        io:format("~n"),
+
+        B
+
+
+        %io:format("~p~n", [Acc]),
+        % B = case string:trim(Action) of 
+        %     "G" ++ _ -> { string:trim(Action), [] };
+        %     _        -> [H|_] = Acc,
+        %                 { G, T } = H,
+        %                 { G, T ++ [Mins] }
+        % end,
+
+%      Acc : [{"Guard #10 begins shift",[5,25,30,55]}]
+
+        % A = case Acc of
+        %     []  -> [B];
+        %     %[C] -> [C] ++ Acc;
+        %      _  -> [_|Ta] = Acc,
+        %             [B] ++ Ta
+        %     end,
+
+
+
     end, [], Parsed),
 
     aoc:print_list(Time).
