23e86e58b953d6cc095f0faad250db65d012cb65
Paul Brown <paul.brown@bjss.com>
2020-10-05 07:02:27 +0100

    7-1

--- 2018/src/day7.erl
+++ 2018/src/day7.erl
@@ -1,12 +1,30 @@
 -module(day7).
 -export([run/0]).
 
-
-
 run() ->
     Input = aoc:readlines("../data/day7.txt"),
-    io:format(Input),
+    Start = lists:map(fun(X) -> 
+         Id = string:tokens(X, " "),
+         {lists:nth(2, Id), lists:nth(8, Id)} 
+            end, Input),
+
+    P1 = next(Start, [], ""),
+
+    io:format("~nPart 1 : ~p~n", [P1]).
+
+
+next([], _, C) -> C;
+next([{A,B}], _, C) -> C ++ A ++ B; 
+next(Actions, Done, P1) ->
+    Pre = lists:map(fun({X,_}) -> X end, Actions),
+    Pst = lists:map(fun({_,Y}) -> Y end, Actions),
+    Can = lists:sort(dedup(lists:filter(fun(X) -> not lists:member(X, Pst) end,Pre))),
+    Next = hd(Can),
+    Action2 = lists:filter(fun({S,_}) -> S /= Next end, Actions),
+    next(Action2, [Next] ++ Done, P1 ++ Next).
 
 
 
-    io:format("~nPart 1 : ~p~n", [0]).
+dedup(L) -> 
+    S = sets:from_list(L),
+    sets:to_list(S).
