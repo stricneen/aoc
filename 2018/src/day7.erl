2e36ebc476e4f89956a804c65607052a635f30b6
Paul Brown <paul.brown@bjss.com>
2020-10-07 08:46:44 +0100

    7-2

--- 2018/src/day7.erl
+++ 2018/src/day7.erl
@@ -2,7 +2,7 @@
 -export([run/0]).
 
 run() ->
-    Input = aoc:readlines("../data/day.txt"),
+    Input = aoc:readlines("../data/day7.txt"),
     Start = lists:map(fun(X) -> 
          Id = string:tokens(X, " "),
          {lists:nth(2, Id), lists:nth(8, Id)} 
@@ -11,99 +11,90 @@ run() ->
      P1 = next(Start, [], ""),
     io:format("~nPart 1 : ~p~n", [P1]),
 
-   
-    % Part 2
-    % Pre = lists:map(fun({X,_}) -> X end, Start),
-    % Pst = lists:map(fun({_,Y}) -> Y end, Start),
-    % Can = lists:sort(aoc:dedup(lists:filter(fun(X) -> not lists:member(X, Pst) end,Pre))),
-    % Remaining = lists:map(fun(X) -> {X, hd(X) - 64} end, Can),
-
-    ThreadCount = 2,
+    ThreadCount = 5,
     Threads = lists:duplicate(ThreadCount, {free}),
-    Time = tick(0, Start, Threads),
-    io:format("~nPart 2 : ~p~n", [Time]),
 
+    StartAll = lists:foldl(fun({X,Y}, Acc) -> [{X,Y}] ++ [{Y,"a"}] ++ Acc end, [], Start),
 
-    io:format("~p~n", [replace(lists:duplicate(5, {free}), ["R", "E", "A", "C", "H", "X"])])
+    Time = tick(0, StartAll, Threads),
 
-
+    io:format("~nPart 2 : ~p~n", [Time])
+    %io:format("~p~n", [replace(lists:duplicate(5, {free}), ["R", "E", "A", "C", "H", "X"])])
 
 .
 
+%  c(day7), day7:run().
 
 tick(Second, State, Threads) ->
+    io:format("~n"),
+    % Remove any state items about to finish
+    Finishing = lists:foldl(fun(X,Acc) -> 
+        R = case X of
+           % {free} -> [];
+            {A,1} -> [A];
+            _ -> []
+        end,
+        R ++ Acc      
+        end, [], Threads),
+
+    io:format("Threads : ~p~n", [Threads]),
+    % io:format("Finishing : ~p~n", [Finishing]),
+
+    NewState = lists:filter(fun({X,_}) -> not lists:any(fun(Y) -> X == Y end, Finishing) end, State),
 
     % Free any complete threads
-    Free = lists:map(fun(T) -> 
+    Tick = lists:map(fun(T) -> 
         R = case T of
             {free} -> {free};
             {_,1} -> {free};
-            X -> X
+            {X,Y} -> {X,Y-1}
         end,
         R
         end, Threads),
 
+    Pre = lists:map(fun({X,_}) -> X end, NewState),
+    Pst = lists:map(fun({_,Y}) -> Y end, NewState),
+
     
-    Pre = lists:map(fun({X,_}) -> X end, State),
-    Pst = lists:map(fun({_,Y}) -> Y end, State),
-    CanStart = lists:sort(aoc:dedup(lists:filter(fun(X) -> not lists:member(X, Pst) end,Pre))),
-    
-    io:format("~p~n", [Free]),
-    io:format("~p~n", [CanStart]),
-
-    % Allocate jobs
-    % NextThreads = lists:foldl(fun(Thread, CS) -> 
-    %     [H|T] = CS,
-    %     R = case T of
-    %         {free} when length(CS) > 0 -> {H, (H - 64)};
-    %         X -> X
-    %     end,
-    %     R
-    %     end, CanStart, Free),
-
-    Bob = replace(Free, CanStart),
+    CanStartQueue = lists:sort(aoc:dedup(lists:filter(fun(X) -> not lists:member(X, Pst) end,Pre))),
+    % Remove anything currently running
 
 
-    Bob.
+    Running = lists:filter(fun(X) -> not (X == {free}) end, Tick),
+    CanStart = lists:filter(fun(X) -> not lists:any(fun({A,_}) -> A == X end, Running) end, CanStartQueue) -- Finishing,
 
-% assign(Threads, Jobs) ->
-    
-%     case Jobs of
-%         [] -> Threads
-%         [H|T] -> case lists:any(fun(X) -> X == {free})
-
-    % Next = case FreeThread and (length(CanStart) > 0) of
-
-    %     true -> State;
+    % io:format("CanStartQueue : ~p~n", [CanStartQueue]),
+    % io:format("Running : ~p~n", [Running]),
+    % io:format("Can start : ~p~n", [CanStart]),
 
 
 
-    %     % tick down running
-    %     false -> lists:map(fun(Thread) -> 
-    %         X = case Thread of
-    %             {free} -> {free};
-    %             {_,1} -> {free};
-    %             {T,C} -> {T,C-1}
-    %         end,
-    %         X end, State)
-    % end,
+    Allocated = replace(Tick, CanStart),
+    %io:format("~p~n", [Allocated]),
+
+    %Running = lists:filter(fun(X) -> not (X == {free}) end, Allocated),
+    %io:format("~p~n", [Running]),
+
+    % NewState = lists:filter(fun({X,_}) -> not lists:any(fun({Y,Z}) -> (X == Y)  end, Running)  end, State),
+    % io:format("State : ~p~n", [NewState]),
+
+
+
+    R = case lists:all(fun(T) -> T == {free} end, Allocated) of
+        true -> Second;
+        false -> tick(Second+1, NewState, Allocated)
+    end,
+
+    R.
 
 
 replace(L,[]) -> L;
-% [free,free]
-% ["C"]
 replace(L1, L2) ->
     [H|T] = L2,
-
-  %  Tuples = lists:map(fun(X) -> {X} end,L1),
-    Replaced = lists:keyreplace(free, 1, L1, {H,((hd(H)-64))}),
+    Replaced = lists:keyreplace(free, 1, L1, {H,((hd(H)-64+60))}),
     replace(Replaced, T).
 
     
-
-
-
-
 
 next([], _, C) -> C;
 next([{A,B}], _, C) -> C ++ A ++ B; 
