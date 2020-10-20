f8df5021afabc781ed670687c5c4aa9e83ba5c03
Paul Brown <paul.brown@bjss.com>
2020-10-20 18:58:56 +0100

    Change tack

--- 2018/src/day8.erl
+++ 2018/src/day8.erl
@@ -1,36 +1,119 @@
 -module(day8).
 -export([run/0]).
+-include_lib("stdlib/include/assert.hrl").
+
 %-record(node, {childCount, metaCount, children, metadata}).
 
 
 run() ->
-    Input2 = hd(aoc:readlines("../data/day8.txt")),
+    % Input2 = hd(aoc:readlines("../data/day8.txt")),
     Input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2",
     L = lists:map(fun(X) -> 
         list_to_integer(X)
         end, string:tokens(Input, " ")),
 
-    %Part 1
+    io:format("In  : ~p~n", [L]),
+
+    % Part 1
     Remaing = rev(L, 0),
     io:format("Part 1 : ~p~n", [Remaing]),
 
-    % Tree = tree(0, L),
-    % io:format("Part 2 A : ~w~n", [Tree]),
+    % X = trav(L, 1),
 
-    % Tree2 = tree(1, Tree),
-    % io:format("Part 2 B : ~w~n", [Tree2])
-%     c(day8), day8:run().
+   
+    % ?assertEqual([0, 3, 10, 11, 12], first_node([0, 3, 10, 11, 12], 0, 0)),
+    % ?assertEqual([0, 3, 10, 11, 12], first_node([0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2], 0, 0)),
+    % ?assertEqual([1, 1, 0, 1, 99, 2], first_node([1, 1, 0, 1, 99, 2, 0, 3, 10, 11, 12], 0, 0)),
 
-    io:format("")
+
+
+    X = first_node(L, 0, 0),
+    io:format("All : ~p~n", [X])
 .
 
 
 
 
+parse_node(L) ->
+
+    Folder = fun(_, {Children, Rest}) ->
+        { Child, NextRest } = parse_node(Rest),
+        { [Child | Children], NextRest } end,
+
+    [NumChild, NumMeta, Inner] = L,
+    { Children, After } = lists:foldr(Folder, { [], Inner}, lists:seq(1, NumChild)),
+    {{ Children, lists:sublist(After, NumMeta) }, lists:nthtail(NumMeta, After) }.
 
 
 
 
+
+
+% In  : [2,3,0,3,10,11,12,1,1,0,1,99,2,1,1,2]
+% All : [2,3,0,3,10,11,12,1,1,0,1,99,  1,1,2]
+
+
+
+%      1 , 1 , 0 , 1 , 99 , 2 , 0 , 3 , 10 , 11 , 12
+
+
+%   1,1,      0,1,99,    2
+
+first_node([], _,  _) -> [];
+
+first_node(L, Depth, MetaCarry) ->
+
+    [ NumChildren | [ NumMeta | T ] ] = L,
+  
+  
+    { Children, Meta } = lists:split(length(L) - NumMeta, L),
+  
+  
+    %{ Meta, Children } = lists:split(NumMeta, T),
+    
+    R = case NumChildren of
+        0 -> [NumChildren] ++ [NumMeta] ++ element(1, lists:split(NumMeta, T));
+        _ -> [NumChildren] ++ [NumMeta] ++ first_node(T, NumChildren - 1, NumMeta)
+        end,
+    
+    case Depth of
+        0 -> R ++ element(1, lists:split(MetaCarry, Meta));
+        _ -> Remain = element(2, lists:split(length(R), Children)),
+             R  ++ first_node(Remain, Depth - 1, 0) ++ Meta
+    end.
+
+
+
+
+
+
+
+%     c(day8), day8:run().
+
+% trav(L, C) ->
+%     [ NumChildren | [ NumMeta | T ] ] = L,
+%     { Children, Meta } = lists:split(length(T) - NumMeta, T),
+
+%     X = split(Children, NumChildren),
+
+%     % io:format("Start : ~p ~p~n", [NumChildren, NumMeta]),
+%     % io:format("Child : ~p ~n", [Children]),
+%     % io:format("Meta  : ~p ~n~n", [Meta]),
+
+%     % R = case NumChildren of 
+%     %     0 -> [Meta];
+%     %     _ -> trav(Children, NumChildren)
+%     % end,
+%     % R.
+%     0.
+
+% split(L, C) ->
+%     F = first_node(L),
+%     io:format("Meta  : ~p ~p~n", [L,C]),
+%     0.
+
+
+%    1 1 0 1 99 2
 
 
 
