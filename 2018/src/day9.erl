72bb904f35baadbea489f44ebbce211303294706
Paul Brown <paul.brown@bjss.com>
2020-10-25 08:51:20 +0000

    9-1

--- 2018/src/day9.erl
+++ 2018/src/day9.erl
@@ -7,22 +7,15 @@
 run() ->
 
     % ?assertEqual(32, play(9, 25)),
-  %   ?assertEqual(8317, play(10, 1618)),  
-    %  ?assertEqual(146373, play(13, 7999)),
+    % ?assertEqual(8317, play(10, 1618)),  
+    % ?assertEqual(146373, play(13, 7999)),
 
-
-
-    io:format("~nPart 1 : ~p~n", [play(9, 4000)]).
-%    io:format("~nPart 1 : ~p~n", [play(431, 70950)]).
-   % io:format("~nPart 2 : ~p~n", [play(431, 7095000)]).
+    io:format("~nPart 1 : ~p~n", [play(431, 70950)]),
+    io:format("~nPart 2 : ~p~n", [play(431, 7095000)]).
 
 play(NPlayers, Marbles) ->
 
-    %Cut = (Marbles + 1) / NPlayers,
-
     Folder = fun(Marble, {Turn, State, Players}) ->
-        
-       
         R = case lists:keyfind(Turn, 1, Players) of
             { Player, Score } -> Next = case Turn of NPlayers -> 1; _ -> Turn + 1 end,
                                  { NextState, S } = move(State, Marble),
@@ -31,20 +24,24 @@ play(NPlayers, Marbles) ->
         end,
     R end,
 
-    {_,_,A} = lists:foldl(Folder, { 1, [0], [ {X, 0} || X <- lists:seq(1,NPlayers) ] }, lists:seq(1, Marbles)),
+    Iq = queue:new(),
+    {_,_,A} = lists:foldl(Folder, { 1, queue:in(0,Iq), [ {X, 0} || X <- lists:seq(1,NPlayers) ] }, lists:seq(1, Marbles)),
     lists:max(lists:map(fun(X) -> element(2, X) end, A))
     .
 
-move([L], Turn) -> {[Turn] ++ [L], 0};
-move(L, Turn) ->
-   io:format("~p~n",[hd(L)]),
+move(Q, Turn) ->
     case Turn rem 23 of
-        0 -> {H,[ExtraScore|U]} = lists:split(length(L)-7, L),
-            % {_,A} = lists:split(length(A) - , U++H),
-             io:format("~p~n",[ExtraScore]),
-            A = U ++ H,
-             {A, Turn + ExtraScore};
-        _ -> [H,G|T] = L,
-             {[Turn] ++ T ++ [H] ++ [G], 0}
+        0 -> 
+            Q1 = cycle(Q, 7),
+            {{_,ExtraScore},R} = queue:out_r(Q1),
+            {R, Turn + ExtraScore};
+        _ -> R = cycle(Q, -2),
+             {queue:in(Turn, R), 0}
     end.
   
+cycle(Q, N) ->
+    case N of
+        N when N > 0 -> {{_,O1}, Q1} = queue:out(Q), cycle(queue:in(O1, Q1), N-1);
+        N when N < 0 -> {{_,O1}, Q1} = queue:out_r(Q), cycle(queue:in_r(O1, Q1), N+1);
+        _ -> Q
+    end.
