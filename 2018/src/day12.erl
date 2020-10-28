f455eb067b380e5df6dfd181fdab624917dac2e2
Paul Brown <paul.brown@bjss.com>
2020-10-28 23:58:07 +0000

    12-1

--- 2018/src/day12.erl
+++ 2018/src/day12.erl
@@ -3,39 +3,39 @@
 
 run() ->
 
-    TPlants = ".....#..#.#..##......###...###.....",
-    TTrans = dict:from_list([{"...##", "#"},
-            {"#.#.#", "#"},
-            {".###.", "."},
-            {"#.#..", "."},
-            {".#..#", "."},
-            {"#..#.", "."},
-            {"..##.", "."},
-            {"....#", "."},
-            {"#....", "."},
-            {"###..", "#"},
-            {".####", "#"},
-            {"###.#", "#"},
-            {"#..##", "."},
-            {".....", "."},
-            {"##.##", "#"},
-            {"####.", "#"},
-            {"##.#.", "#"},
-            {"#...#", "."},
-            {"#####", "."},
-            {"..#..", "#"},
-            {".#.#.", "#"},
-            {"#.###", "#"},
-            {".##.#", "."},%
-            {"..#.#", "."},
-            {".#.##", "#"},
-            {"...#.", "."},
-            {"##...", "."},
-            {"##..#", "."},
-            {".##..", "#"},
-            {".#...", "#"},
-            {"#.##.", "."},
-            {"..###", "."}]),
+    %TPlants = ".....#..#.#..##......###...###.....",
+    % TTrans = dict:from_list([{"...##", "#"},
+    %         {"#.#.#", "#"},
+    %         {".###.", "."},
+    %         {"#.#..", "."},
+    %         {".#..#", "."},
+    %         {"#..#.", "."},
+    %         {"..##.", "."},
+    %         {"....#", "."},
+    %         {"#....", "."},
+    %         {"###..", "#"},
+    %         {".####", "#"},
+    %         {"###.#", "#"},
+    %         {"#..##", "."},
+    %         {".....", "."},
+    %         {"##.##", "#"},
+    %         {"####.", "#"},
+    %         {"##.#.", "#"},
+    %         {"#...#", "."},
+    %         {"#####", "."},
+    %         {"..#..", "#"},
+    %         {".#.#.", "#"},
+    %         {"#.###", "#"},
+    %         {".##.#", "."},%
+    %         {"..#.#", "."},
+    %         {".#.##", "#"},
+    %         {"...#.", "."},
+    %         {"##...", "."},
+    %         {"##..#", "."},
+    %         {".##..", "#"},
+    %         {".#...", "#"},
+    %         {"#.##.", "."},
+    %         {"..###", "."}]),
 
     Plants = ".....##.#.#.##..#....######..#..#...#.#..#.#.#..###.#.#.#..#..###.##.#..#.##.##.#.####..##...##..#..##.#...",
     Trans = dict:from_list([{"...##", "#"},
@@ -78,12 +78,34 @@ run() ->
     
     
     
-    io:format("~p~n", [Plants]),
+    %io:format("~p~n", [Plants]),
 
-    %Gen = gen(Plants,Trans, 19),
-    Gen = gen(Plants,Trans, 19),
-    %io:format("~p~n~n", [Gen]),
-    io:format("Part 1 : ~p~n", [score(Gen)])
+    Gen = gen(Plants,Trans, 20),
+    io:format("Part 1 : ~p~n", [score(Gen)]),
+
+    P2Input = 50000000000,
+    io:format("Part 1 : ~p~n", [((P2Input+10) * 38) +4])
+
+    %=((D10+10) * 38) +4
+    % io:format("~p\t~p~n", [200, score(gen(Plants,Trans, 200))]),
+    % io:format("~p\t~p~n", [300, score(gen(Plants,Trans, 300))]),
+
+
+    % io:format("~p\t~p~n", [400, score(gen(Plants,Trans, 400))]),
+
+    % io:format("~p\t~p~n", [500, score(gen(Plants,Trans, 500))]),
+    % io:format("~p\t~p~n", [501, score(gen(Plants,Trans, 501))]),
+    % io:format("~p\t~p~n", [502, score(gen(Plants,Trans, 502))]),
+    % io:format("~p\t~p~n", [503, score(gen(Plants,Trans, 503))]),
+    % io:format("~p\t~p~n", [504, score(gen(Plants,Trans, 504))]),
+
+
+    % io:format("~p\t~p~n", [400, score(gen(Plants,Trans, 400))]),
+    % io:format("~p\t~p~n", [401, score(gen(Plants,Trans, 401))]),
+    % io:format("~p\t~p~n", [402, score(gen(Plants,Trans, 402))]),
+    % io:format("~p\t~p~n", [403, score(gen(Plants,Trans, 403))]),
+    % io:format("~p\t~p~n", [404, score(gen(Plants,Trans, 404))])
+
 .
 
 score(L) ->
@@ -98,13 +120,10 @@ score(L) ->
        R end,
       Scoring, lists:seq(Offset, length(Scoring)+Offset-1)),
 
-    io:format("~p~n", [Scores]),
+    %io:format("~p~n", [Scores]),
     lists:sum(Scores).
 
 
-% 1677 tl 2119
-
-% th 2206
 
 gen(Plants, Trans, C) -> 
      Folder = fun(Plant, N) ->
@@ -117,10 +136,10 @@ gen(Plants, Trans, C) ->
         "..",
         lists:seq(0, length(Plants)-5) ) ++ "...",
     
-    io:format("~p~n", [Gen]),
+    %io:format("~p~n", [Gen]),
     
     case C of 
-        0 -> Gen;
+        1 -> Gen;
         _ -> gen(Gen, Trans, C-1)
     end.
     
---
+++ 2018/src/day13.erl
@@ -0,0 +1,11 @@
+-module(day13).
+-export([run/0]).
+
+
+
+run() ->
+    Input = aoc:readlines("../data/day13.txt"),
+    io:format(Input),
+
+    io:format("~nPart 1 : ~p~n", [0]).
+
