6645389ccc38795fba1c206e0c35312d5c2324be
Paul Brown <paul.brown@bjss.com>
2020-11-03 20:11:32 +0000

    14-2

--- 2018/src/day14.erl
+++ 2018/src/day14.erl
@@ -7,15 +7,19 @@ run() ->
     L = [3 , 7 , 1 , 0 ,1, 0, 1],
     E1 = 1,
     E2 = 3,
+    
+    
+    %Receipes = [5,9,4,1,4],
+    
     Receipes = [0,7,7,2,0,1],
-    %Receipes = [5,9,4],
+    %Receipes = [  1,  6,  7,  7,  9,2 ],
    
 % 51589 first appears after 9 recipes.
 % 01245 first appears after 5 recipes.
 % 92510 first appears after 18 recipes.
 % 59414 first appears after 2018 recipes.
 
-    A = step(lists:reverse(L), E1, E2, lists:reverse(Receipes),0),
+    A = step(lists:reverse(L), E1, E2, lists:reverse(Receipes), length(L)),
     
     % {_,B} = lists:split(Receipes, A),
     % {C,_} = lists:split(10, B),
@@ -36,34 +40,23 @@ step(L, E1, E2, Receipes,C) ->
     Next = [list_to_integer([Char]) || Char <- Stringed] ++ L,
 
 % io:format("~p~n", [lists:reverse(Next)]),
+    Len = C + length( [list_to_integer([Char]) || Char <- Stringed]),
 
-    Ne1 = mk_p((E1 - Er1+ length(Stringed) - 1), length(Next)  ) ,
-    Ne2 = mk_p((E2 - Er2+ length(Stringed) - 1), length(Next) ) ,
+    Ne1 = mk_p((E1 - Er1+ length(Stringed) - 1), Len) ,
+    Ne2 = mk_p((E2 - Er2+ length(Stringed) - 1), Len) ,
    % Last1 = lists:nthtail(length(Next)-6, Next),
 
-% io:format("~p~n", [Last1]),
-% io:format("~p~n", [Last2]),
-
-% c(day14), day14:run().
-
-    % case length(Next) > 20 of
-    %     true -> Next;
-    %     false -> step(Next, 
-    %                 Ne1 ,
-    %                 Ne2 ,
-    %                 Receipes,C+1)
-    %     end.
-
+   case C rem 10000 of
+       0 -> io:format("~p~n", [C]); _ -> 0 end,
 
     case (lists:prefix(Receipes, L)) of
-        true -> length(L) - length(Receipes);
+        true -> C - length(Receipes);
 
              false -> step(Next, 
                     Ne1 ,
                     Ne2 ,
-                    Receipes,C+1)
+                    Receipes,Len)
     end.
-
 
 mk_p(V, A) ->
     if V < 1 -> V + A; true -> V end.
---
+++ 2018/src/day15.erl
@@ -0,0 +1,8 @@
+-module(day15).
+-export([run/0]).
+
+run() ->
+    Input = aoc:readlines("../data/day.txt"),
+    io:format(Input),
+
+    io:format("~nPart 1 : ~p~n", [0]).
