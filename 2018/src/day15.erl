dbe5d1444f4db020f911ccb1e56bddb3265f9feb
Paul Brown <paul.brown@bjss.com>
2020-11-07 14:45:17 +0000

    15-s

--- 2018/src/day15.erl
+++ 2018/src/day15.erl
@@ -1,8 +1,35 @@
 -module(day15).
 -export([run/0]).
 
+% # 35  . 46      E 69  G 71
+
 run() ->
     Input = aoc:readlines("../data/day.txt"),
-    io:format(Input),
 
-    io:format("~nPart 1 : ~p~n", [0]).
+    Folder = fun(NLine, Acc) -> 
+        Line = lists:nth(NLine, Input),
+        Dic = lists:zipwith(fun(X,Y) -> {{Y,NLine}, X} end, Line, lists:seq(1,length(Line))),
+        NoSp = lists:filter(fun({_,X}) -> X /= 32 end, Dic),
+        A = dict:from_list(NoSp),
+        dict:merge(fun(X)->X end, Acc,A)
+        end,
+    Grid = lists:foldl(Folder, dict:new(), lists:seq(1, length(Input))),
+    
+    %Positions = dict:filter(fun(_,V) -> (V==69) or (V==71) end, Grid),
+    Board = dict:map(fun(_,V) -> 
+        case V of
+            69 -> {elf, 200};
+            71 -> {gob, 200};
+            35 -> {wal, 0};
+            46 -> {spc, 0};
+            _ -> {} 
+        end
+        end, Grid),
+
+    % determine move
+
+
+    io:format("~nPart 1 : ~p~n", [Board]).
+
+    % move
+    % attach
