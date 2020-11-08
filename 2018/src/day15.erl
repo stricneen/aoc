c3ec2d7ba7009124b11d9b4bee3c0c66746ccd54
Paul Brown <paul.brown@bjss.com>
2020-11-08 10:15:56 +0000

    simple path

--- 2018/src/day15.erl
+++ 2018/src/day15.erl
@@ -26,10 +26,69 @@ run() ->
         end
         end, Grid),
 
-    % determine move
+    Players = dict:filter(fun(_,{X, _}) -> (X =:= elf) or (X =:= gob) end, Board),
+   
+   % determine move
+
+ io:format("~nPart 1 : ~p~n", [Board]),
+
+   First = play_round(Board),
+
+ io:format("~nPart 1 : ~p~n", ["First"]).
 
 
-    io:format("~nPart 1 : ~p~n", [Board]).
+
+
 
     % move
     % attach
+
+play_round(Board) ->
+    Players = dict:filter(fun(_,{X, _}) -> (X =:= elf) or (X =:= gob) end, Board),
+    PlayersL = lists:sort(dict:fold(fun(K,V,Acc) -> [{K,V}] ++ Acc end,  [], Players)),
+    io:format("~n Players : ~p~n", [PlayersL]),
+    Tick = lists:map(fun(X) -> turn(X, Board) end, PlayersL),
+    Tick.
+
+turn(Player, Board) ->
+    io:format("~n Player : ~p~n", [Player]),
+    {{X,Y}, {Ty,_}} = Player,
+
+    Boundary = path(Ty, [{X,Y}] ,Board, 1),
+    io:format("  Moves : ~p~n", [Boundary]),
+    Boundary.
+
+path(Ty, Pos ,Board, Dist) ->
+
+    % {{X,Y}, {T,_}} = Player,
+
+    Spread = lists:foldl(fun({X,Y}, Acc) -> 
+        [{{X, Y-1} ,dict:find({X, Y-1}, Board), Dist},
+        {{X, Y+1}, dict:find({X, Y+1}, Board), Dist},
+        {{X-1, Y}, dict:find({X-1, Y}, Board), Dist},
+        {{X+1, Y}, dict:find({X+1, Y}, Board), Dist}] ++ Acc
+         end, [], Pos),
+         
+
+    Boundary = lists:foldl(fun({{X,Y}, {_,{Ty,S}}, D}, Acc) -> 
+                R = case (Ty =:= wal) or (lists:any(fun({X1,Y1}) -> (X1==X) and (Y1==Y) end, Pos) ) of
+                    true -> Acc;
+                    false -> [{{X,Y},{Ty,S},D}] ++ Acc
+                end, R end, [] ,Spread),
+
+    
+
+    Enemy = case Ty of 
+        gob -> elf;
+        _ -> gob
+    end,
+    
+     case lists:any(fun({_,{Ty,_},_}) -> Ty =:= Enemy end, Boundary) or (Dist > 20) of
+         true -> Boundary;
+         false -> path(Ty, lists:map(fun ({{X,Y}, {Ty,S}, D}) -> {X,Y} end, Boundary), Board, Dist+1)
+     end.
+
+%    Boundary.
+   
+
+    
