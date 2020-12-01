1b3372775fbb60ffc9dbe59e3027b8c63492c29a
Paul Brown <paul.brown@bjss.com>
2020-12-01 09:20:01 +0000

    day 2 setup

--- 2020/day01.js
+++ 2020/day01.js
@@ -1,6 +1,4 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const aoc = require('./aoc');
-    
 const buffer = aoc.readfile('day01.txt');
 
 const text = buffer.split(/\n/);
@@ -31,18 +29,3 @@ for (var a = 0; a < l.length; a++)
                 console.log("Part 2 : ", l[a] * l[b] * l[c]);
             }
 
-
-
-
-
-
-
-// const calc = input.map(x => (Math.floor(x/3))-2);
-// const p1 = aoc.sum(calc);
-// console.log("Part 1 ", p1);     // 3394689
-
-
-// const calcFuel = (x) => x < 9 ? 0 : (Math.floor(x/3))-2 + calcFuel((Math.floor(x/3))-2);
-// const calc2 = input.map(x => calcFuel(x));
-// var p2 = aoc.sum(calc2);
-// console.log("Part 2 ", p2);     // 5089160
