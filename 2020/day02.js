381b78e48ae5c6a13d3f405e6e7b77d16ba3bde7
Paul Brown <paul.brown@bjss.com>
2020-12-02 05:11:17 +0000

    2-1

--- 2020/day02.js
+++ 2020/day02.js
@@ -3,9 +3,35 @@ const aoc = require('./aoc');
 const buffer = aoc.readfile('day02.txt');
 
 const text = buffer.split(/\n/);
-const input = text.map(x => parseInt(x));
+const input = text.map(x => {
+
+    var s = x.split(' ');
+    var m = s[0].split('-');
+
+    var l = s[1][0];
 
 
+    return {min: parseInt(m[0]), max: parseInt(m[1]), l: l, pw: s[2]};
+});
 
-console.log(input);
+
+var valids = input.map(x => {
+
+    const count = x.pw.split('').filter(y => y == x.l).length;
+
+    // console.log(count);
+
+    return (count >= x.min && count <= x.max) ;
+
+    
+
+    console.log(x);
+    console.log(valid);
+
+});
+// 17-20 k: wbbbcqlwlvphdsqckwbm
+
+var count = valids.filter(x => x);
+
+console.log(count.length);
     
