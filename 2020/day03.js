7430b4049c6884f04bfcac7d94637d6e3374c81f
Paul Brown <paul.brown@bjss.com>
2020-12-02 05:58:59 +0000

    3s

---
+++ 2020/day03.js
@@ -0,0 +1,14 @@
+const aoc = require('./aoc');
+const buffer = aoc.readfile('day03.txt');
+const text = buffer.split(/\n/);
+const input = text.map(x => {
+    return parseInt(x);
+});
+
+
+console.log("Part 1", input);
+
+
+
+console.log("Part 2", "...");
+    
