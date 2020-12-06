7dee8419853026d22b60e7bf8ced443cfb8e6da9
Paul Brown <paul.brown@bjss.com>
2020-12-06 05:29:28 +0000

    7-1

--- 2020/day06.js
+++ 2020/day06.js
@@ -3,9 +3,35 @@ const aoc = require('./aoc');
 const buffer = aoc.readfile('day06.txt');
 
 const text = buffer.split(/\n/);
-const input = text.map(x => parseInt(x));
+// const input = text.map(x => parseInt(x));
+// 5348
+const group = text.reduce((a,x) => {
+
+        if (x.length == 0) {
+            a.push("");
+            return a;
+        } else {
+             a[a.length-1] += x;
+             return a;
+        }
+
+
+}, [""]);
 
 
 
-console.log(input);
+const dedup = group.map(x => {
+    var y = x.split('');
+    var z = [...new Set(y)];
+
+    return z.sort().join('');
+});
+
+console.log(dedup);
+
+const len = dedup.map(x => x.length);
+
+console.log(len);
+
+console.log(aoc.sum(len));
     
