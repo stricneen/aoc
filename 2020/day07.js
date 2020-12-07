67ef2cfe0132f00ad8945cfd29b43ccf9b263cfd
Paul Brown <paul.brown@bjss.com>
2020-12-07 08:49:06 +0000

    7-2

--- 2020/day07.js
+++ 2020/day07.js
@@ -1,7 +1,4 @@
-const { constants } = require('buffer');
-const { allowedNodeEnvironmentFlags, rawListeners } = require('process');
 const aoc = require('./aoc');
-    
 const buffer = aoc.readfile('day07.txt');
 
 const text = buffer.split(/\n/);
@@ -43,30 +40,50 @@ const canContain = (l) => {
 };
 
 const shiny = (l, acc) => {
-
     const cc = canContain(l);
     if (cc.length == 0) return acc;
-
-
     var accn = acc.concat(cc);
     var n = shiny(cc.map(x=>x.bag), accn);
     return n;   
 }
 
 const r = shiny(['shiny gold'], []);
-
 const names = r.map(x => x.bag);
-
-console.log('-----');
 console.log("Part 1 : ", [...new Set(names)].length);
 
 
-// const b = canContain(['shiny gold']);
+const addBags = (l, acc) => {
+
+    // if (l.length==0) return acc;
+    // find bags in l
+    const b = bags.filter(x => l.includes(x.bag));
+    const c = b.map(x => x.inside);
+    const merged = [].concat.apply([], c).filter(x => x.bag != 'o other');
+
+    if (merged.length == 0) return 0;
+
+    console.log("merged" ,merged);
+
+    const layer = merged.map(x => x.n + (x.n * addBags(x.bag, 0)));
 
 
-// outer: 'vibrant plum',
-// inside: [
-//   { n: 5, bag: 'faded blue bags' },
-//   { n: 6, bag: 'dotted black bags.' }
-// ]
-// }
+
+    console.log("Layer", layer);
+
+    const total = aoc.sum(layer);
+
+    console.log("Total", total);
+
+    return acc + total;
+    // const next = merged.map(x => x.bag);
+    // console.log("next", next);
+
+    // return total + addBags(next, 0);
+}
+
+
+// Gold bag 
+
+const gold = bags.filter(x => x.bag == 'shiny gold');
+
+console.log(addBags('shiny gold', 0));
