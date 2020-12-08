9ea91cd36bfb9a932a81e9d3b1767687a9677ac5
Paul Brown <paul.brown@bjss.com>
2020-12-08 05:10:47 +0000

    8-1

--- 2020/day08.js
+++ 2020/day08.js
@@ -1,11 +1,42 @@
 const aoc = require('./aoc');
     
-const buffer = aoc.readfile('day08.txt');
+const buffer = aoc.readfile('day.txt');
 
 const text = buffer.split(/\n/);
-const input = text.map(x => parseInt(x));
+
+const instr = text.map(x => {
+    return {i: x.split(' ')[0], v: parseInt(x.split(' ')[1]) }
+});
+
+var acc = 0;
+var ptr = 0;
+
+var exec = [];
+
+while (true) {
+    var curr = instr[ptr];
+
+    if (exec.includes(ptr)) {
+        break;
+    }
+
+    exec.push(ptr);
+
+    console.log(curr);
+    if (curr.i == 'nop') {
+    }
+    if (curr.i == 'acc') {
+        acc += curr.v;
+    }
+    if (curr.i == 'jmp') {
+        ptr += curr.v-1;
+    }
+    
+    ptr ++;
+console.log(acc, ptr);
+
+}
 
 
-
-console.log(input);
+console.log(acc);
     
