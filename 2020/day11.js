ce2ebd3d4c3e160c1629beefd8fd31754817d6ee
Paul Brown <paul.brown@bjss.com>
2020-12-11 16:51:45 +0000

    11-2

--- 2020/day11.js
+++ 2020/day11.js
@@ -32,7 +32,7 @@ const vat = (grid, coord) => {
      return grid[coord.y]?.[coord.x];
 }
 
-const surrounding = (grid, coord) => {
+const surrounding1 = (grid, coord) => {
 
     const s = [
         grid[coord.y - 1]?.[coord.x -1],
@@ -50,7 +50,44 @@ const surrounding = (grid, coord) => {
     return s;
 };
 
-const part1 = () => {
+// Infinate path generator
+const path =  function* (start, vector) {
+    let current = start;
+    while(true) {
+        current = {x: current.x + vector.x, y: current.y + vector.y };
+        yield current;
+    }
+};
+
+const surrounding2 = (grid, coord) => {
+    // What can coord see ?
+    // What can up see ?
+    const vectors = [ {x:-1, y:-1}, {x:-1, y:0},{x:-1, y:1},
+                    {x:0, y:-1},               {x:0, y:1},
+                    {x:1, y:-1}, {x:1, y:0},{x:1, y:1} ];
+    
+    return vectors.reduce((a,e) => {
+        
+        for (let c of path(coord, e)) {
+
+            const scan = vat(grid, c);
+            
+            // console.log(scan);
+            if (scan === undefined) return a;
+            if (scan == '#') {
+                a.push('#');
+                return a;
+            }
+            if (scan == 'L') {
+                a.push('L');
+                return a;
+            } 
+        }
+    }, []);              
+                
+};
+
+const run = (surroundFunc, friends) => {
     var i = 0;
     let loop = grid;
     while (true) {
@@ -58,12 +95,12 @@ const part1 = () => {
         for (let c of allCoords(width, height)) {
 
             const curr = vat(loop, c);
-            const s = surrounding(loop, c);
+            const s = surroundFunc(loop, c);
             if (curr == '.') {
                 next += '.';
             } else if (curr == 'L' && s.filter(x => x == '#').length == 0) {
                 next += '#';
-            } else if (curr == '#' && s.filter(x => x == '#').length >= 4) {
+            } else if (curr == '#' && s.filter(x => x == '#').length >= friends) {
                 next += 'L';
             } else {
                 next += curr;
@@ -75,7 +112,9 @@ const part1 = () => {
         }
         
         loop = stringToGrid(next, width, height);
+      
     }
 };
 
-console.log("Part 1 : ", part1());
+console.log("Part 1 : ", run(surrounding1, 4));
+console.log("Part 2 : ", run(surrounding2, 5));
