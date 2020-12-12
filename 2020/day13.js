6098320b3ae4cd248f9a4d0241af79dda8fd47ff
Paul Brown <paul.brown@bjss.com>
2020-12-12 07:12:13 +0000

    13s

---
+++ 2020/day13.js
@@ -0,0 +1,9 @@
+const aoc = require('./aoc');
+const buffer = aoc.readfile('day13.txt');
+const text = buffer.split(/\n/);
+const input = text.map(x => parseInt(x));
+
+
+
+console.log(input);
+    
