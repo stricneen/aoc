2a2caaa839fe5ef31a79c97ef3e8410c0dc798e4
Paul Brown <paul.brown@bjss.com>
2020-12-12 06:34:53 +0000

    12-2 tidy

--- 2020/day12.js
+++ 2020/day12.js
@@ -2,66 +2,33 @@ const { maxHeaderSize } = require('http');
 const aoc = require('./aoc');
 const buffer = aoc.readfile('day12.txt');
 const text = buffer.split(/\n/);
-const input = text.map(x => parseInt(x));
-
-// console.log(text);
 
 const dirs = text.map(x => {return {d:x[0], dist: parseInt(x.substring(1))} });
-console.log(dirs);
     
-
-pos = { x: 0, y:0 };
-var  facing = 'e';
-waypoint = { x: 10, y: -1};
-
-dirs.map(e => {
-    console.log();
-console.log(e);
-console.log("Facing", facing);
-
+const p2 = dirs.reduce((a,e) => {
+    
     if (e.d == 'F') {
-        pos =  {x: pos.x + waypoint.x * e.dist,         y:  pos.y + waypoint.y * e.dist}
-     
+        return { ...a,  pos: {x: a.pos.x + a.waypoint.x * e.dist, y:  a.pos.y + a.waypoint.y * e.dist }};
     }
 
     if (e.d == 'R') {
-
-        if (e.dist == 90) waypoint = {x: -waypoint.y, y: waypoint.x};
-        if (e.dist == 180) waypoint = {x: -waypoint.x, y: -waypoint.y};
-        if (e.dist == 270) waypoint = {x: waypoint.y, y: -waypoint.x};
-
+        if (e.dist == 90)  return { ...a,  waypoint: {x: -a.waypoint.y, y: a.waypoint.x}};
+        if (e.dist == 180) return { ...a,  waypoint: {x: -a.waypoint.x, y: -a.waypoint.y}};
+        if (e.dist == 270) return { ...a,  waypoint: {x: a.waypoint.y, y: -a.waypoint.x}};
     }
  
     if (e.d == 'L') {
-        if (e.dist == 90) waypoint = {x: waypoint.y, y: -waypoint.x};
-        if (e.dist == 180) waypoint = {x: -waypoint.x, y: -waypoint.y};
-        if (e.dist == 270) waypoint = {x: -waypoint.y, y: waypoint.x};
-
-
-
+        if (e.dist == 90)  return { ...a,  waypoint: {x: a.waypoint.y, y: -a.waypoint.x}};
+        if (e.dist == 180) return { ...a,  waypoint: {x: -a.waypoint.x, y: -a.waypoint.y}};
+        if (e.dist == 270) return { ...a,  waypoint: {x: -a.waypoint.y, y: a.waypoint.x}};
     }
-    if (e.d == 'N') {
-        waypoint = {x: waypoint.x, y: waypoint.y - e.dist}
-    }
+
+    if (e.d == 'N') return { ...a, waypoint: {x: a.waypoint.x, y: a.waypoint.y - e.dist}};    
+    if (e.d == 'S') return { ...a, waypoint: {x:a.waypoint.x, y: a.waypoint.y + e.dist}};
+    if (e.d == 'E') return { ...a, waypoint: {x:a.waypoint.x + e.dist, y: a.waypoint.y}};
+    if (e.d == 'W') return { ...a, waypoint: {x:a.waypoint.x - e.dist, y: a.waypoint.y}};
     
-    if (e.d == 'S') {
-        waypoint = {x:waypoint.x, y: waypoint.y + e.dist}
-    }
-    if (e.d == 'E') {
-        waypoint = {x:waypoint.x + e.dist, y: waypoint.y};
-    }
-    
-    if (e.d == 'W') {
-        waypoint = {x:waypoint.x -e.dist, y: waypoint.y}
-    }
-    
-    //2998
-console.log(pos);
-    
+ 
+}, { pos: {x:0, y:0}, waypoint:{x: 10, y: -1}, facing: 'e'} );
 
-//710
-})
-
-console.log("Part 1  : " , Math.abs(pos.x) + Math.abs(pos.y));
-
-// console.log(pos);
+console.log("Part 2  : " , Math.abs(p2.pos.x) + Math.abs(p2.pos.y));
