a3ac7e5d5f87b10b7a181cc5c89ce873629484f1
Paul Brown <paul.brown@bjss.com>
2020-12-15 07:00:18 +0000

    15tidy

--- 2020/day15.js
+++ 2020/day15.js
@@ -3,7 +3,7 @@ const input = "0,13,1,8,6,15".split(",")
 const solve = (n) => {
   let lastNum = input[input.length - 1];
   const lastSpoken = new Array(n);
-  input.forEach((v, i) => lastSpoken[v] = i + 1);
+  input.forEach((e,i) => lastSpoken[e] = i + 1);
 
   for (let i = input.length; i < n; i++) {
     const next = lastSpoken[lastNum] ? i - lastSpoken[lastNum] : 0;
