15e4afd9fa790320882bd01579adc983d5df55d4
Paul Brown <paul.brown@bjss.com>
2020-12-18 05:49:22 +0000

    18-1

--- 2020/day17.js
+++ 2020/day17.js
@@ -1,10 +1,9 @@
-const { isMainThread } = require('worker_threads');
 const aoc = require('./aoc');
-const buffer = aoc.readfile('day17.txt');
+const buffer = aoc.readfile('day.txt');
 
 const text = buffer.split(/\n/);
 
-const hash = (obj) => (obj.w * 1000000000) + (obj.x * 1000000) + (obj.y * 10000) + obj.z;
+const hash = (obj) => (obj.w * 1000000) + (obj.x * 10000) + (obj.y * 100) + obj.z;
 
 let iy = 0;
 let grid = [];
@@ -17,38 +16,29 @@ for (l of text) {
     iy++;
 }
 
-
-
-const around = function*(p)  {
-
+const neighbours = function*(p)  {
     for(let w= -1; w < 2; w++)
-    for(let z= -1; z < 2; z++)
-        for(let y = -1; y < 2; y++)
-            for(let x = -1; x < 2; x++) {
-
-                if (x == 0 && y == 0 && z == 0 && w == 0) {
-                } else {
-                    yield {x:x + p.x, y:y + p.y, z:z + p.z, w:w + p.w}
+        for(let z= -1; z < 2; z++)
+            for(let y = -1; y < 2; y++)
+                for(let x = -1; x < 2; x++) {
+                    if (x != 0 || y != 0 || z != 0 || w != 0) {
+                        yield {x:x + p.x, y:y + p.y, z:z + p.z, w:w + p.w}
+                    }
                 }
-            }
-
-
-
 };
 
 const surround = (grid,p) => {
     let active= 0;
-    for (s of around(p))
+    for (s of neighbours(p))
         if (grid.includes(hash(s))) {
             active++;
         }
     return active;
 }
 
-
 const cycles = 6;
-let size = text[0].length + 2;
-
+let size = text[0].length + 1;
+console.time('x');
 let next = [];
 for(let cycle = 0; cycle < cycles; cycle++) {
 
@@ -75,14 +65,4 @@ for(let cycle = 0; cycle < cycles; cycle++) {
     grid = next;
     next = [];
 }
-
-
-
-
-
-// for (let c of around({x:0,y:0,z:0})) {
-//     console.log(c);
-// }
-
-
-
+console.timeEnd('x');
---
+++ 2020/day18.js
@@ -0,0 +1,71 @@
+const aoc = require('./aoc');
+    
+const buffer = aoc.readfile('day18.txt');
+
+const text = buffer.split(/\n/);
+
+
+// 1 + 2 * 3 + 4 * 5 + 6 
+// 2 * 3 + (4 * 5)
+// 5 + (8 * 3 + 9 + 3 * 4 * 3)
+// 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+// ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+
+
+
+const solve = (eq) => {
+
+    console.log(eq);
+
+
+    if (aoc.isNumber(eq)) {
+        return parseInt(eq);
+    }
+
+    if (eq.indexOf(')') > -1) {
+
+        const firstC = eq.indexOf(')');
+        const prev = eq.substring(0, firstC);
+        const matchO = prev.lastIndexOf('(');
+
+        const inner = eq.substr(matchO + 1, firstC - 1);
+        const closing = inner.indexOf(')');
+
+        const before = eq.substring(0, matchO);
+        const innereq = closing == -1 ? inner : inner.substring(0, closing);
+        const after = eq.substring(firstC + 1);
+
+
+        // ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+        return solve(before + solve(innereq) + after);
+    }
+    else {
+        const part = eq.split(' ');
+        if (part[1] == '*') {
+            const m = parseInt(part[0]) * parseInt(part[2]);
+            const n  = m.toString() + ' ' + part.slice(3).join(' ');
+            return solve(n);
+        }
+        if (part[1] == '+'){
+            const m = parseInt(part[0]) + parseInt(part[2]);
+            const n  = m.toString() + ' ' + part.slice(3).join(' ');
+            return solve(n);
+        }
+
+    }
+
+
+
+    return parseInt(eq);
+};
+
+let ans = [];
+for (eq of text) {
+    let a = solve(eq);
+    ans.push(a);
+    console.log(a);
+}
+
+console.log("Part 1 : ", aoc.sum(ans));
+
+    
