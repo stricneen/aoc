6c630f917e411e9f27ea4cd7d90f68ca3dcc18a9
Paul Brown <paul.brown@bjss.com>
2021-01-09 16:57:03 +0000

    18-1-paper

--- 2018/src/day20.erl
+++ 2018/src/day20.erl
@@ -1,47 +1,73 @@
 -module(day20).
 -export([run/0]).
 -include_lib("stdlib/include/assert.hrl").
-
+% th 4198
+% tl 4169
 run() ->
-    Input = aoc:readlines("../data/day20.txt"),
     tests(),
-    io:format("~nPart 1 : ~p~n", [0]).
+    
+    Input = hd(aoc:readlines("../data/day20.txt")),
+    P1 = calc_dist(Input),
+    io:format("~nPart 1 : ~p~n", [P1]).
 
 calc_dist(Route) ->
-    Clean = lists:reverse(tl(lists:reverse(tl(Route)))),
-    % Clean = string:replace(string:replace(Route, "^", ""), "$", ""),
-    io:format("~p~n", [Clean]),
-    dist(Clean).
+    C = lists:filter(fun(X) -> (X =/= 94) and (X =/= 36) end, Route),
+    T = lists:foldl(fun(X, { A, [D|O] }) ->
+        case X of 
+            78 ->  { [ {X, D, {0,  1}} ] ++ A, [ D + 1 | O ] };  % N
+            83 ->  { [ {X, D, {0, -1}} ] ++ A, [ D + 1 | O ] };  % S
+            69 ->  { [ {X, D, {1,  0}} ] ++ A, [ D + 1 | O ] };  % E
+            87 ->  { [ {X, D, {-1, 0}} ] ++ A, [ D + 1 | O ] };  % W
+            40  -> { A, [ D ] ++ [ D | O ] };     % (
+            41  -> { A,  O  };     % )
+            124 -> { A, [ hd(O) | O ] }    % |
+        end
+        end, {[], [1]}, C),
 
-dist([]) -> 0;
-dist(Route) ->
-    io:format("Route : ~p\t~n", [Route]), 
-    case lists:member($(, Route) of
-        false ->  length(Route);
-        true -> {Pre, Bracket, Remainder} = split(Route),
-         io:format("Splitting : ~p~n", [Route]),
-         io:format("Pre : ~p\t~n", [Pre]), 
-         io:format("Bra : ~p\t~n", [Bracket]), 
-         io:format("Rem : ~p\t~n", [Remainder]), 
-                dist(Pre) + dist(Bracket) + dist(Remainder)
+    
+    Path1 = lists:foldl(fun({_,_,{X,Y}},A) -> 
+        {I,J} = hd(A),
+        [{ I+X, J+Y }] ++ A
+        end, [{0,0}], lists:reverse(element(1, T))),
+
+    DropOrigin = lists:reverse(tl(lists:reverse(Path1))),
+    
+    % io:format("Path1 : ~p~n", [ Path1 ]),
+    
+    Path = lists:zip(DropOrigin, element(1, T)),
+
+    io:format("Path : ~p~n", [Path]),
+
+    Dists = lists:foldl(fun({{I,J},{_,X,_}},A) ->
+        [{{I,J},X}] ++ A 
+    end, [], Path),
+
+    % io:format("Dists : ~p~n", [Dists]),
+
+
+
+
+    Normals = lists:reverse(lists:sort(lists:map(fun({X,Y}) -> {Y,X} end, Dists))),     
+       
+    io:format("~p~n", [lists:filter(fun({Y,_}) -> Y >= 4168 end, Normals)]),
+    % io:format("~nNormals : ~p~n", [Normals]),
+    % 4184
+
+  io:format("~p~n", [lists:filter(fun({_,Y}) -> Y =:= {16,49} end, Normals)]),
+  
+
+    longest(Normals).
+    
+longest([{X,Y}|T]) ->
+    case lists:any(fun({_,Y1}) -> Y =:= Y1  end, T) of
+        false -> X;
+        true -> longest(T)
     end.
 
-split(Route) ->
-    [H|T] = string:split(Route, "("),
-    % io:format("split : ~p\t~n", [T]), 
-    {A,B} = match(hd(T), [], 1),
-    % io:format("Pre : ~p\t Inner : ~p\t Post : ~p~n", [H,A,B]), 
-    { H, A, B }.
-
-match([$)|T], Acc, 1) -> {Acc, T};
-match(L1, L2, 0) -> {L1, L2};
-match([$(|T], Acc, Depth) -> match(T, Acc ++ [$(], Depth + 1);
-match([$)|T], Acc, Depth) -> match(T, Acc ++ [$)], Depth - 1);
-match(R, Acc, Depth) -> 
-    [H|T] = R,
-    % io:format("R : ~p\t~n", [R]), 
-    match(T, Acc ++ [H], Depth).
     
+
+
+
 tests() -> 
     test({"^WNE$", 3}),
     test({"^ENWWW(NEEE|SSE(EE|N))$", 10}),
@@ -51,3 +77,4 @@ tests() ->
 
 test({Route, Dist}) ->
     ?assertEqual(Dist, calc_dist(Route), Route).
+
