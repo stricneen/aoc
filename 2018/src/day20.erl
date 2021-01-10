60c19e68fdb6dada2884e29b568fd473dbffad08
Paul Brown <paul.brown@bjss.com>
2021-01-10 10:53:51 +0000

    20-1

--- 2018/src/day20.erl
+++ 2018/src/day20.erl
@@ -8,50 +8,38 @@ run() ->
     
     Input = hd(aoc:readlines("../data/day20.txt")),
     
-    %  io:format("~nPart 1 : ~p~n", [ calc_dist(Input)]),
-
+    io:format("~nPart 1 : ~p~n", [ calc_dist(Input)]),
     io:format("~nPart 1 : ~p~n", [0]).
 
 calc_dist(Route) ->
     C = lists:filter(fun(X) -> (X =/= 94) and (X =/= 36) end, Route),
-    T = lists:foldl(fun(X, { A, [D|O] }) ->
+
+    T = lists:foldl(fun(X, { A, [ {D,{PX,PY}} |O] }) ->
         case X of 
-            78 ->  { [ {X, D, {0, -1}} ] ++ A, [ D + 1 | O ] };  % N
-            83 ->  { [ {X, D, {0,  1}} ] ++ A, [ D + 1 | O ] };  % S
-            69 ->  { [ {X, D, {1,  0}} ] ++ A, [ D + 1 | O ] };  % E
-            87 ->  { [ {X, D, {-1, 0}} ] ++ A, [ D + 1 | O ] };  % W
-            40  -> { A, [ D ] ++ [ D | O ] };     % (
-            41  -> { A,  O  };     % )
-            124 -> { A, [ hd(O) | O ] }    % |
+            78 ->  { [ {X, D, {PX, PY-1}} ] ++ A, [ {D+1, {PX,PY-1}} | O ] };  % N
+            83 ->  { [ {X, D, {PX, PY+1}} ] ++ A, [ {D+1, {PX,PY+1}} | O ] };  % S
+            69 ->  { [ {X, D, {PX+1, PY}} ] ++ A, [ {D+1, {PX+1,PY}} | O ] };  % E
+            87 ->  { [ {X, D, {PX-1, PY}} ] ++ A, [ {D+1, {PX-1,PY}} | O ] };  % W
+      
+            40  -> { A, [ {D, {PX,PY}} ] ++ [ {D, {PX,PY}} | O ] };                    % (
+            41  -> { A, O };                                    % )
+            124 -> { A, [ hd(O) | O ] }                          % |
+      
         end
-        end, { [], [1] }, C),
+        end, { [], [{1, {0,0}}] }, C),
 
-    
-    Path1 = lists:foldl(fun({_,_,{X,Y}},A) -> 
-        {I,J} = hd(A),
-        [{ I+X, J+Y }] ++ A
-        end, [{0,0}], lists:reverse(element(1, T))),
+ io:format("T : ~p~n", [T]),
 
-    DropOrigin = lists:reverse(tl(lists:reverse(Path1))),
-    Path = lists:zip(DropOrigin, element(1, T)),
-
-    io:format("Path : ~p~n", [Path]),
-
-    Dists = lists:foldl(fun({{I,J},{_,X,_}},A) ->
-        [{{I,J},X}] ++ A 
-    end, [], Path),
-    
-    Normals = lists:reverse(lists:map(fun({X,Y}) -> {Y,X} end, Dists)),     
-       
-print(lists:reverse(Normals)),
-    % io:format("~p~n", [lists:filter(fun({Y,_}) -> Y >= 4168 end, Normals)]),
-    % io:format("~nNormals : ~p~n", [Normals]),
-    % 4184
-
-%   io:format("~p~n", [lists:filter(fun({_,Y}) -> Y =:= {16,49} end, Normals)]),
-  
-
+    Normals = lists:reverse(lists:sort(lists:map(fun({_,D,{X,Y}}) -> {D,{X,Y}} end, element(1, T)))),
+    % io:format("Normals : ~p~n", [Normals]),
     longest(Normals).
+
+    
+longest([{X,Y}|T]) ->
+    case lists:any(fun({_,Y1}) -> Y =:= Y1  end, T) of
+        false -> X;
+        true -> longest(T)
+    end.
     
 print(L) ->
     io:format(os:cmd(clear)), % clear screen
@@ -62,18 +50,14 @@ print(L) ->
     end, L),
     aoc:print(20,0, "").
 
-longest([{X,Y}|T]) ->
-    case lists:any(fun({_,Y1}) -> Y =:= Y1  end, T) of
-        false -> X;
-        true -> longest(T)
-    end.
+
 
 tests() -> 
-    % test({"^WNE$", 3}),
-    test({"^ENWWW(NEEE|SSE(EE|N))$", 10}).
-    % test({"^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$", 18}),
-    % test({"^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$", 23}),
-    % test({"^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$", 31}).
+    test({"^WNE$", 3}),
+    test({"^ENWWW(NEEE|SSE(EE|N))$", 10}),
+    test({"^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$", 18}),
+    test({"^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$", 23}),
+    test({"^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$", 31}).
 
 test({Route, Dist}) ->
     ?assertEqual(Dist, calc_dist(Route), Route).
