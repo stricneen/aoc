557af2ba986130ab092953fb45ba19ea736d6461
Paul Brown <paul.brown@bjss.com>
2021-01-10 11:25:58 +0000

    20-2

--- 2018/src/day20.erl
+++ 2018/src/day20.erl
@@ -8,8 +8,9 @@ run() ->
     
     Input = hd(aoc:readlines("../data/day20.txt")),
     
-    io:format("~nPart 1 : ~p~n", [ calc_dist(Input)]),
-    io:format("~nPart 1 : ~p~n", [0]).
+    Rooms = calc_dist(Input),
+    io:format("~nPart 1 : ~p~n", [ longest(Rooms) ]),
+    io:format("~nPart 2 : ~p~n", [ length(lists:filter(fun({D,_}) -> D >= 1000 end, Rooms))]).
 
 calc_dist(Route) ->
     C = lists:filter(fun(X) -> (X =/= 94) and (X =/= 36) end, Route),
@@ -20,37 +21,35 @@ calc_dist(Route) ->
             83 ->  { [ {X, D, {PX, PY+1}} ] ++ A, [ {D+1, {PX,PY+1}} | O ] };  % S
             69 ->  { [ {X, D, {PX+1, PY}} ] ++ A, [ {D+1, {PX+1,PY}} | O ] };  % E
             87 ->  { [ {X, D, {PX-1, PY}} ] ++ A, [ {D+1, {PX-1,PY}} | O ] };  % W
-      
-            40  -> { A, [ {D, {PX,PY}} ] ++ [ {D, {PX,PY}} | O ] };                    % (
-            41  -> { A, O };                                    % )
-            124 -> { A, [ hd(O) | O ] }                          % |
+            40  -> { A, [ {D, {PX,PY}} ] ++ [ {D, {PX,PY}} | O ] };            % (
+            41  -> { A, O };                                                   % )
+            124 -> { A, [ hd(O) | O ] }                                        % |
       
         end
         end, { [], [{1, {0,0}}] }, C),
 
- io:format("T : ~p~n", [T]),
-
     Normals = lists:reverse(lists:sort(lists:map(fun({_,D,{X,Y}}) -> {D,{X,Y}} end, element(1, T)))),
-    % io:format("Normals : ~p~n", [Normals]),
-    longest(Normals).
+    normalise(Normals, []).
 
-    
-longest([{X,Y}|T]) ->
-    case lists:any(fun({_,Y1}) -> Y =:= Y1  end, T) of
-        false -> X;
-        true -> longest(T)
+normalise([], N) -> N;
+normalise([{D,Room}|T], N) ->
+    case lists:any(fun({_,R}) -> R =:= Room end, T) of
+        true -> normalise(T,N);
+        false -> normalise(T,[{D,Room}] ++ N)
     end.
+
+
+longest(L) ->
+    lists:max(lists:map(fun({D,_}) -> D end, L)).
     
-print(L) ->
-    io:format(os:cmd(clear)), % clear screen
-    aoc:print(10, 10, "O"),
-    lists:foreach(fun({D,{X,Y}}) -> 
-        timer:sleep(100),
-        aoc:print(X+10, Y+10, "*")
-    end, L),
-    aoc:print(20,0, "").
-
-
+% print(L) ->
+%     io:format(os:cmd(clear)), % clear screen
+%     aoc:print(10, 10, "O"),
+%     lists:foreach(fun({D,{X,Y}}) -> 
+%         timer:sleep(100),
+%         aoc:print(X+10, Y+10, "*")
+%     end, L),
+%     aoc:print(20,0, "").
 
 tests() -> 
     test({"^WNE$", 3}),
@@ -60,5 +59,5 @@ tests() ->
     test({"^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$", 31}).
 
 test({Route, Dist}) ->
-    ?assertEqual(Dist, calc_dist(Route), Route).
+    ?assertEqual(Dist, longest(calc_dist(Route)), Route).
 
