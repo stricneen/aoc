c39d5b356186cdb39bafa740ce9bf4065225b736
Paul Brown <paul.brown@bjss.com>
2021-01-24 21:32:21 +0000

    22-a

--- 2018/src/day23.erl
+++ 2018/src/day23.erl
@@ -1,9 +1,8 @@
 -module(day23).
 -export([run/0]).
 
-
 run() ->
-    Input = aoc:readlines("../data/day.txt"),
+    Input = aoc:readlines("../data/day23.txt"),
     
     Bots = lists:map(fun(E) -> 
         T = string:tokens(E, " <>,="),
@@ -12,7 +11,12 @@ run() ->
           list_to_integer(lists:nth(4,T)),
           list_to_integer(lists:nth(6,T)) } end, Input),
     
-    io:format("~p~n", [Bots]),
+    % io:format("~p~n", [Bots]),
 
+    Largest = hd(lists:reverse(lists:keysort(4, Bots))),
+    InRange = lists:filter(fun(X) -> dist(Largest, X) =< element(4,Largest) end, Bots),
 
-    io:format("~nPart 1 : ~p~n", [0]).
+    io:format("~nPart 1 : ~p~n", [length(InRange)]).
+
+dist({Xs, Ys, Zs, _}, {Xt, Yt, Zt, _}) ->
+    abs(Xs - Xt) + abs(Ys - Yt) + abs(Zs - Zt).
