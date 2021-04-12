1fb9e013fe1b20fc3301cee4a65d6775294253fa
Paul Brown <paul.brown@bjss.com>
2021-04-12 23:22:05 +0100

    25 tidy - what to do now?

--- 2018/src/day25.erl
+++ 2018/src/day25.erl
@@ -5,44 +5,29 @@ run() ->
     Input = aoc:readlines("../data/day25.txt"),
     Parsed = parse(Input),
     
-    % Dists = [ {A,B,dist(A,B)} || A <- Parsed, B <-Parsed ],
-    A = constellation(Parsed, []),
-      io:format(" ~p~n", [A]),
+    A = constellation(Parsed),
     io:format("~nPart 1 : ~p~n", [length(A)]).
 
-constellation(L, C) -> 
-
-    io:format("~p~n", [L]),
-
+constellation(L) -> 
     T = lists:foldl(fun(E,A) -> 
-         io:format("Acc: ~p~n", [A]),
-
+        
         Distances = lists:map(fun(X) -> 
             lists:map(fun(Y) -> {dist(Y,E), Y} end, X) end, A),
-
-%   io:format("Dists: ~p~n", [Distances]),
-
 
         { Connections , Others } = lists:partition(
             fun(X) -> lists:any(fun({D,_}) -> D =< 3 end, X)
             end, Distances),
 
+        Conn = lists:map(fun(Lx) -> lists:map(fun({_,Cx}) -> Cx end, Lx) end,Connections),
+        Othe = lists:map(fun(Lx) -> lists:map(fun({_,Cx}) -> Cx end, Lx) end,Others),
 
-%   io:format("{ Connections , Others }: ~p~n", [{ Connections , Others }]),
-
-Conn = lists:map(fun(Lx) -> lists:map(fun({_,Cx}) -> Cx end, Lx) end,Connections),
-Othe = lists:map(fun(Lx) -> lists:map(fun({_,Cx}) -> Cx end, Lx) end,Others),
-
-%   io:format("~p~n", [{ Conn , Othe }]),
         merge(E, Conn, Othe)
-    
     end, [], L),
     
     T.
 
 merge(E, [], O) -> [[E]] ++ O;
 merge(E, C, O) -> [[E|lists:append(C)]] ++ O.
-
 
 dist({A1,A2,A3,A4},{B1,B2,B3,B4}) -> 
     abs(A1-B1) + abs(A2-B2) + abs(A3-B3) + abs(A4-B4).
