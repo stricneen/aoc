bad2997ce331ccc7f45d1d266de0b081b08283fa
Paul Brown <paul.brown@krakenflex.com>
2021-12-02 09:12:16 +0000

    chore: wip

---
+++ 2021/day03.js
@@ -0,0 +1,25 @@
+const aoc = require('./aoc');
+const buffer = aoc.readfile('day03.txt');
+
+const text = buffer.split(/\n/);
+const moves = text.map(x => {
+  const r = x.split(' ');
+  return { cmd: r[0], dist: parseInt(r[1]) }
+})
+
+const [h, d] = moves.reduce(([horz, depth], e) => {
+  if (e.cmd === 'down') return [horz, depth + e.dist];
+  if (e.cmd === 'up') return [horz, depth - e.dist];
+  if (e.cmd === 'forward') return [horz + e.dist, depth];
+}, [0,0]);
+
+console.log("Part 1 : ", h * d);
+
+
+const [horz, depth,] = moves.reduce(([horz, depth, aim], e) => {
+  if (e.cmd === 'down') return [horz, depth, aim + e.dist];
+  if (e.cmd === 'up') return [horz, depth, aim - e.dist];
+  if (e.cmd === 'forward') return [horz + e.dist, depth + (aim * e.dist), aim];
+}, [0, 0, 0]);
+
+console.log("Part 2 : ", horz * depth);
