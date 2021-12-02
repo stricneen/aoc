b01fb5104f7f6f9897fe4e2def8a4c8e25674123
Paul Brown <paul.brown@krakenflex.com>
2021-12-02 08:30:49 +0000

    chore: wip

--- 2021/day02.js
+++ 2021/day02.js
@@ -2,17 +2,26 @@ const aoc = require('./aoc');
 const buffer = aoc.readfile('day02.txt');
 
 const text = buffer.split(/\n/);
-const nums = text.map(x => parseInt(x));
+const moves = text.map(x => {
+  const r = x.split(' ');
+  return { cmd: r[0], dist: parseInt(r[1]) }
+})
 
-let p1 = 0;
-let p2 = 0;
-for (var a = 0; a < nums.length - 3; a++)
-  if (nums[a + 1] > nums[a]) p1 += 1
+const [h, d] = moves.reduce(([horz, depth], e) => {
+  if (e.cmd === 'down') return [horz, depth + e.dist];
+  if (e.cmd === 'up') return [horz, depth - e.dist];
+  if (e.cmd === 'forward') return [horz + e.dist, depth];
+}, [0,0]);
 
-  
+console.log("Part 1 : ", h * d);
+console.assert(h * d === 1488669)
 
-  
-  
-  
-console.log("Part 1 : ", p1);
-console.log("Part 2 : ", p2);
+
+
+const [horz, depth,] = moves.reduce(([horz, depth, aim], e) => {
+  if (e.cmd === 'down') return [horz, depth, aim + e.dist];
+  if (e.cmd === 'up') return [horz, depth, aim - e.dist];
+  if (e.cmd === 'forward') return [horz + e.dist, depth + (aim * e.dist), aim];
+}, [0, 0, 0]);
+
+console.log("Part 2 : ", horz * depth);
