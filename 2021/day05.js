53f676076f8cdaa4fd2b842c22acb63ad27eca76
Paul Brown <paul.brown@krakenflex.com>
2021-12-05 10:46:01 +0000

    feat: 5-2

--- 2021/day05.js
+++ 2021/day05.js
@@ -1,6 +1,5 @@
 const aoc = require('./aoc');
 const buffer = aoc.readfile('day05.txt');
-
 const text = buffer.split(/\n/);
 
 const pos = text.map(x => {
@@ -10,72 +9,36 @@ const pos = text.map(x => {
     y1: parseInt(r[0].split(',')[1]),
     x2: parseInt(r[1].split(',')[0]),
     y2: parseInt(r[1].split(',')[1])
-
   }
-
 })
 
-const axis = pos.filter((p) => p.x1 === p.x2 || p.y1 === p.y2);
+const trace = (coors) => {
+  const map = {};
+  for (const p of coors) {
+    const keys = [];
+    const xd = (p.x1 === p.x2) ? 0 : (p.x1 > p.x2) ? -1 : 1;
+    const yd = (p.y1 === p.y2) ? 0 : (p.y1 > p.y2) ? -1 : 1;
 
-// for (const aa of pos) {
-//   console.log(aa)
-// }
+    let x = p.x1;
+    let y = p.y1;
 
-const map = {};
+    keys.push(`${x}_${y}`);
+    while (x !== p.x2 || y !== p.y2) {
+      x = x + xd;
+      y = y + yd;
+      keys.push(`${x}_${y}`);
+    }
 
-for (const p of axis) {
-  // console.log(p);
-
-
-  if (p.x1 === p.x2) {
-
-    const [a, b] = ([p.y1, p.y2].sort(function (a, b) {  return a - b;  }));
-//    console.log(a,b)
-    for (let i = a; i <= b; i++) {
-      const key = `${p.x1}_${i}`;
-      if (map[key]) {
-        map[key] = map[key] + 1;
-      } else {
-        map[key] = 1
-      }
+    for (const key of keys) {
+      map[key] = (map[key] || 0) + 1
     }
   }
-
-
-  // 7,7 -> 7,7
-  //  console.log('x')
-  else if (p.y1 === p.y2) {
-
-    const x = [p.x1, p.x2].sort(function (a, b) {  return a - b;  });
-    const [a, b] = ([p.x1, p.x2].sort(function (a, b) {  return a - b;  }));
-    //console.log(x)
-    for (let i = a; i <= b; i++) {
-      const key = `${i}_${p.y1}`;
-      if (map[key]) {
-        map[key] = map[key] + 1;
-      } else {
-        map[key] = 1
-      }
-    }
-  } else { // diag
-
-    
-
-
-  }
-
-  //console.log()
+  
+  return map;
 }
 
+const map1 = trace(pos.filter((p) => p.x1 === p.x2 || p.y1 === p.y2));
+console.log('Part 1 : ', Object.keys(map1).filter(x => map1[x] > 1).length)
 
-
-
-
-const c = Object.keys(map).filter(x => map[x] > 1)
-
-// 2: 6007
-//console.log(map)
-console.log(c.length)
-// console.log(Object.keys(map).length)
-//3428
- /// 3338 
+const map2 = trace(pos);
+console.log('Part 2 : ', Object.keys(map2).filter(x => map2[x] > 1).length)
