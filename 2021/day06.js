a96079ab0fbbe652e316ab61c1608637cfb8389a
Paul Brown <paul.brown@krakenflex.com>
2021-12-06 05:52:19 +0000

    feat: 6-2

--- 2021/day06.js
+++ 2021/day06.js
@@ -4,56 +4,27 @@ const text = buffer.split(',');
 const nums = text.map(x => parseInt(x))
 
 const days = 256;
+const levels = [0,0,0,0,0,0,0,0,0];
+nums.forEach(n => levels[n] = levels[n] + 1);
 
-// 26984457539
-
-const newl = () => {
-return  {
-  '0': 0,
-  '1': 0,
-  '2': 0,
-  '3': 0,
-  '4': 0,
-  '5': 0,
-  '6': 0,
-  '7': 0,
-  '8': 0,
-}
-}
-
-const levels = newl();
-
-nums.forEach(x => {
-  levels[x.toString()] = levels[x.toString()] + 1;
-});
-
-
-
-const tick = (levels) => {
-  const next = newl();
-  next['0'] = levels['1'];
-  next['1'] = levels['2'];
-  next['2'] = levels['3'];
-  next['3'] = levels['4'];
-  next['4'] = levels['5'];
-  next['5'] = levels['6'];
-  next['6'] = levels['7'];
-  next['7'] = levels['8'];
-
-  next['6'] = next['6'] + levels['0'];
-  next['8'] = next['8'] + levels['0'];
-  
-  return next;
+const tick = (fish) => {
+  const babies = fish.shift();
+  fish[6] = fish[6] + babies;
+  fish[8] = babies;
+  return fish;
 };
 
-let d = levels
-for(i=0;i<days;i++) {
+let d = [...levels];
+for(i=0;i<80;i++) {
    d = tick(d);
 }
 
-let c = 0;
-Object.keys(d).forEach(x => c = c + d[x])
+console.log('Part 1 : ', aoc.sum(d));
 
-console.log(d);
-console.log(c);
-console.assert(c === 1757714216975)
+d = [...levels];
+for(i=0;i<256;i++) {
+   d = tick(d);
+}
+
+console.log('Part 2 : ', aoc.sum(d));
+
