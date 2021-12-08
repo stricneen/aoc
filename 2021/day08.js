b923c153ecc7437b67637aad5f1cd71105f4b6a5
Paul Brown <paul.brown@krakenflex.com>
2021-12-08 06:37:10 +0000

    chore: wip

---
+++ 2021/day08.js
@@ -0,0 +1,41 @@
+const aoc = require('./aoc');
+const buffer = aoc.readfile('day08.txt');
+const text = buffer.split(/\n/).map(x => x);
+
+const nums = text.map(x => {
+  const t = x.split(' | ');
+  return { a: t[0].split(' '), b: t[1].split(' ')};
+});
+
+ const convert = (segs) => {
+  const one = segs.find(x => x.length === 2);
+  const seven = segs.find(x => x.length === 3);
+  const four = segs.find(x => x.length === 4);
+  const eight = segs.find(x => x.length === 7);
+  const six = segs.filter(x => x.length === 6).filter(x => x.indexOf(one[0]) === -1 || x.indexOf(one[1]) === -1)[0]; //6
+  const three = segs.filter(x => x.length === 5).filter(x => x.indexOf(one[0]) > -1 && x.indexOf(one[1]) > -1)[0]; //5
+  const nine = segs.filter(x => x.length === 6).filter(x => x.indexOf(four[0]) > -1 && x.indexOf(four[1]) > -1 && x.indexOf(four[2]) > -1 && x.indexOf(four[3]) > -1 )[0]; // 6  
+  const zero = segs.filter(x => x.length === 6).filter(x => x !==six && x!==nine )[0]; // 6
+  const five = segs.filter(x => x.length === 5).filter(x => x !== three).filter(x => six.indexOf(x[0]) > -1 && six.indexOf(x[1]) > -1 && six.indexOf(x[2]) > -1 && six.indexOf(x[3]) > -1 && six.indexOf(x[4]) > -1)[0];
+  const two = segs.filter(x => x.length === 5).filter(x => x !== five && x!==three )[0]; // 6
+  return[zero, one,two,three,four,five,six,seven,eight,nine];
+};
+
+const decode = (code, num) => {
+  const c = code.map(x => x.split('').sort().join(''));
+  const n = num.map(x => x.split('').sort().join(''));
+  const x = parseInt(n.map(x => c.indexOf(x)).join(''));
+  return x;
+}
+
+const p1 = nums.reduce((a,e) => {
+  const t = e.b.map(x=>x.length);
+  const x = t.filter(y=>[2,3,4,7].includes(y)).length;
+  return a + x;
+}, 0);
+
+const lines = nums.map(n => decode(convert(n.a), n.b));
+const p2 = aoc.sum(lines);
+
+console.log('Part 1 : ', p1);
+console.log('Part 2 : ', p2);
