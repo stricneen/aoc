96003cc933a8a246324716ef2c83fb25ac22bf16
Paul Brown <paul.brown@krakenflex.com>
2021-12-10 05:56:46 +0000

    chore: wip

--- 2021/day10.js
+++ 2021/day10.js
@@ -1,18 +1,15 @@
 const aoc = require('./aoc');
-const buffer = aoc.readfile('day.txt');
+const buffer = aoc.readfile('day10.txt');
 const text = buffer.split(/\n/);
 
 const scores = {
   ')': 3, ']': 57, '}': 1197, '>': 25137
 }
-
-const openers = '{[<(';
-const closers = ')}]>';
+const openers = '([{<';
+const pairs = ['{}', '[]', '<>', '()'];
 
 const calcPoints = (line, stack) => {
-//xconsole.log()
- // console.log(line, stack)
-  if (line.length === 0) return 0;
+  if (line.length === 0) return [0, stack];
   const arr = line.split('');
   const first = arr.shift();
 
@@ -22,28 +19,16 @@ const calcPoints = (line, stack) => {
   } else {
 
     const c = stack.shift();
-    if (first === ')' && c === '('){
-      return calcPoints(arr.join(''), stack);
-    }
-    if (first === '}' && c === '{'){
-      return calcPoints(arr.join(''), stack);
-    }
-    if (first === ']' && c === '['){
-      return calcPoints(arr.join(''), stack);
-    }
-    if (first === '>' && c === '<'){
-      return calcPoints(arr.join(''), stack);
-    }
-    return scores[first]
+    if (pairs.includes(`${c}${first}`)) return calcPoints(arr.join(''), stack);
+    return [scores[first], stack];
   }
 }
 
 const points = text.map(x => calcPoints(x, []));
-const p1 = aoc.sum(points);
+const p1 = aoc.sum(points.map((x => x[0])));
 console.log('Part 1 : ', p1);
 
-const incomplete = text.filter(x => calcPoints(x, []) === 0);
-
-console.log(incomplete)
-
-console.log('Part 2 : ', 0);
+const incomplete = points.filter(x => x[0] === 0).map(x => x[1]);
+const p = incomplete.map(x => x.reduce((a,e) => (a * 5) + openers.indexOf(e) + 1, 0));
+const s = aoc.sort_ints(p);
+console.log('Part 2 : ', s[Math.floor(s.length/2)])
