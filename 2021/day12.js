b4b1ddd120194b96ab30e8277becc3d0cb84ca9f
Paul Brown <paul.brown@krakenflex.com>
2021-12-12 07:17:03 +0000

    chore: wip

--- 2021/day12.js
+++ 2021/day12.js
@@ -1,39 +1,36 @@
 const aoc = require('./aoc');
-const buffer = aoc.readfile('day12.txt');
+const buffer = aoc.readfile('day.txt');
 const text = buffer.split(/\n/);
 const tunnels = text.map(x => x.split('-')).map(y => ({ from: y[0], to: y[1] }));
-
-const toPath = (path) => {
-  const a = path.map(x => x.from);
-  return [...a, path[path.length - 1].to];
-}
 
 const pathCount = (tunnels, paths, allowPair) => {
 
   if (paths.length === 0) {
     return pathCount([...tunnels, ...tunnels.map(x => ({ from: x.to, to: x.from }))],
-      [...tunnels.filter(x => x.from === 'start').map(x => [x]),
-      ...tunnels.filter(x => x.to === 'start').map(x => ([{ from: x.to, to: x.from }]))], allowPair);
+      [...tunnels.filter(x => x.from === 'start').map(x => ['start', x.to]),
+      ...tunnels.filter(x => x.to === 'start').map(x => (['start', x.from]))], allowPair);
   }
 
-  if (paths.every(x => x[x.length - 1].to === 'end')) {
+  if (paths.every(x => x[x.length - 1] === 'end')) {
     return paths;
   }
 
-  const next = paths.filter(x => x[x.length - 1].to === 'end');
+  const next = paths.filter(x => x[x.length - 1] === 'end');
 
-  paths.filter(x => x[x.length - 1].to !== 'end').forEach(p => {
-    tunnels.filter(x => x.from === p[p.length - 1].to && x.to !== 'start' && x.from !== 'start').forEach(y => {
-      const path = toPath(p);
+  paths.filter(x => x[x.length - 1] !== 'end').forEach(path => {
+    tunnels.filter(x => x.from === path[path.length - 1] && x.to !== 'start' && x.from !== 'start').forEach(tunnel => {
+      
+
       const lc = path.filter(x => x === x.toLowerCase());
       const hasPair = (lc.length !== aoc.dedup(lc).length) || !allowPair;
 
-      if ((y.to == y.to.toLowerCase() && (!path.includes(y.to)) || !hasPair) || y.to === y.to.toUpperCase()) {
-        next.push([...p, y]);
+      if ((tunnel.to == tunnel.to.toLowerCase() && (!path.includes(tunnel.to)) || !hasPair) || tunnel.to === tunnel.to.toUpperCase()) {
+        
+      // console.log('ad')
+        next.push([...path, tunnel.to]);
       }
     });
   });
-
   return pathCount(tunnels, next, allowPair);
 }
 
