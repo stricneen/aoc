d8040c892533d3643163097a3d8584bd0b921cfc
Paul Brown <paul.brown@krakenflex.com>
2021-12-23 12:13:58 +0000

    chore: wip

--- 2021/day23.js
+++ 2021/day23.js
@@ -6,39 +6,28 @@ const aoc = require('./aoc');
 //   #B#C#D#A#
 //   #########
 
-              1
-// #01 3 5 7 90#
-// #...........#
-// ###B#C#B#D###
-//   #A#D#C#A#
-//   #########
-
-//  const moves = {
-//      aout: [
-//          [0,[0,1,2]],
-//          [1,[1,2]],
-//          [3,[2,3]],
-//          [5,[2,3,4,5]],
-//          [7,[2,3,4,5,6,7]],
-//          [9,[2,3,4,5,6,7,8,9]],
-//          [10,[2,3,4,5,6,7,8,9,10]],
-//      ]
-//  }
-
-const moves = [0,1,3,5,7,9,10];
-
-  const start = [{
-    a: ['A','B'],
-    b: ['D','C'],
-    c: ['C','B'],
-    d: ['A','D'],
+  const p1 = [{
+    a: ['B', 'C'],
+    b: ['C', 'B'],
+    c: ['D', 'A'],
+    d: ['A', 'D'],
     corr: Array(11).fill(null),
     energy: 0
-  }]
+}];
 
-  aoc.pj(start)
+const moves = [0, 1, 3, 5, 7, 9, 10];
 
-const isDone = state => 
+const test = [{
+    a: ['A', 'B'],
+    b: ['D', 'C'],
+    c: ['C', 'B'],
+    d: ['A', 'D'],
+    corr: Array(11).fill(null),
+    energy: 0,
+    route: []
+}]
+
+const isDone = state =>
     state.a[0] === 'A' &&
     state.a[1] === 'A' &&
     state.b[0] === 'B' &&
@@ -48,67 +37,133 @@ const isDone = state =>
     state.d[0] === 'D' &&
     state.d[1] === 'D';
 
-  let min = Number.MAX_VALUE;
+let min = Number.MAX_VALUE;
 
- const isclearPath = (corr, s ,e) => {
-     return corr.slice(Math.min(s,e),Math.max(s,e)).every(x => x === null)
- }
+const isclearPath = (corr, s, e) => {
+    return corr.slice(Math.min(s, e), Math.max(s, e)).every(x => x === null)
+}
 
- const energy = {'A':1,'B':10,'C':100,'D':1000}
+const energy = { 'A': 1, 'B': 10, 'C': 100, 'D': 1000 }
 
-  const tick = (states, c) => {
-    if (c===0) {return states}
+const u = [['A', 2], ['B', 4], ['C', 6], ['D', 8]];
+
+
+const tick = (states, c) => {
+    if (c === 0) { return states }
+    if (states.length === 0) return { states}
+    console.log(states.length);
     const next = [];
-    // calc all next states
 
-    for (const state of states) {
+    for (let cx = 0; cx < states.length; cx++) {
+        const state = states[cx];
+
         if (state.energy > min) continue;
         if (isDone(state)) {
-             if (state.energy < min) min = state.energy;
+            console.log('done - ', state.energy)
+            if (state.energy < min) min = state.energy;
             continue;
         }
 
-        // a: ['A','B'],
-        // b: ['D','C'],
-        // c: ['C','B'],
-        // d: ['A','D'],
-        // corr: Array(11).fill(null),
+
+        // Move out of first room
+        for (const [home, out] of u) {
+            if ((state[home.toLowerCase()][1] !== home || state[home.toLowerCase()][0] !== home) && state[home.toLowerCase()][1] !== '') {
+                for (const move of moves) {
+                    if (isclearPath(state.corr, out, move)) {
+                        const c = [...state.corr];
+                        c[move] = state[home.toLowerCase()][1];
+                        next.push({
+                            ...state,
+                            [home.toLowerCase()]: [state[home.toLowerCase()][0], ''],
+                            energy: state.energy + (energy[home] * (Math.abs(out - move)+1)),
+                            corr: c,
+                        })
+                    }
+                }
+            }
+        }
+
+        //Move out of second room
+        for (const [home, out] of u) {
+            if ((state[home.toLowerCase()][0] !== home)
+                && state[home.toLowerCase()][0] !== ''
+                && state[home.toLowerCase()][1] === '') {
+                for (const move of moves) {
+                    if (isclearPath(state.corr, out, move)) {
+                        const c = [...state.corr];
+                        c[move] = state[home.toLowerCase()][0];
+                        next.push({
+                            ...state,
+                            [home.toLowerCase()]: ['', ''],
+                            energy: state.energy + (energy[home] * (Math.abs(out - move) + 2)),
+                            corr: c,
+                            // parent: JSON.stringify(state)
+                        })
+                    }
+                }
+            }
+        }
 
 
-        // const out = 2; // door
-        // const home = 'A';
+          // move into first room
+           // [['A', 2], ['B', 4], ['C', 6], ['D', 8]];
+        for (const [home, out] of u) {
+            if ((state[home.toLowerCase()][0] === home)
+                && state[home.toLowerCase()][1] === '') {
+                for (let i = 0; i < state.corr.length; i++) {
+                    const move = state.corr[i];
+                    if (move === home) {
+                        const tempcorr = [...state.corr];
+                        tempcorr[i] = null;
 
-        const u = [['A',2],['B',4],['C',6],['D',8]];
-    for (const [home,out] of u)     
+                        if (isclearPath(tempcorr, out, i)) {
+                   
+                            next.push({
+                                ...state,
+                                [home.toLowerCase()]: [home, home],
+                                energy: state.energy + (energy[home] * (Math.abs(out - i)+1 )),
+                                corr: tempcorr,
+                                // parent: JSON.stringify(state)
+                            })
 
-        if ((state[home.toLowerCase()][1] !== home || state[home.toLowerCase()][0])
-        && state[home.toLowerCase()][1] !== '') {
-           
-            for (const move of moves) {
-                if (isclearPath(state.corr,out,move)) {
-                    const c = [...state.corr];
-                    c[move] = state[home.toLowerCase()][1];
-                    next.push({
-                        ...state,
-                        [home.toLowerCase()]: [state[home.toLowerCase()][0], ''],
-                        energy: state.energy + (energy[state[home.toLowerCase()][1]] * Math.abs(out-move)),
-                        corr: c
-                    })
+                        }
+                    }
+                }
+            }
+        }
+
+        // move into second room
+        for (const [home, out] of u) {
+            if (state[home.toLowerCase()][0] === ''
+                && state[home.toLowerCase()][1] === '') {
+                for (let i = 0; i < state.corr.length; i++) {
+                    const move = state.corr[i];
+                    if (move === home) {
+                        const tempcorr = [...state.corr];
+                        tempcorr[i] = null;
+                        if (isclearPath(tempcorr, out, i)) {
+                            next.push({
+                                ...state,
+                                [home.toLowerCase()]: [home, ''],
+                                energy: state.energy + (energy[home] * (Math.abs(out - i) + 2)),
+                                corr: tempcorr,
+                            })
+                        }
+                    }
                 }
             }
         }
     }
 
 
+    const str = next.map(x => JSON.stringify(x));
+    const obj = [...new Set(str)].map(x => JSON.parse(x));
 
-    // remove any over min
+    console.log(str.length, obj.length);
 
-    // any done? check againt min & discard
+    return tick(obj, c - 1)
+}
 
-aoc.pj(next)
-console.log('')
-    return tick(next,c-1)
-  }
-
-  tick(start,1);
-  console.log('Part 1 : ', min);
+tick(test, 20)
+// console.log(tick(start, 5));
+console.log('Part 1 : ', min);
