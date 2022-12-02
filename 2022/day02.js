ddfb2a29334cf7cd9cb14440b96dc32a03e70eab
Paul Brown <paul.brown@krakenflex.com>
2022-12-02 08:43:07 +0000

    feat: day2

--- 2022/day02.js
+++ 2022/day02.js
@@ -2,11 +2,29 @@ const aoc = require('./aoc');
 const buffer = aoc.readfile('day02.txt');
 
 const text = buffer.split(/\n/);
-const nums = text.map(x => parseInt(x));
+const lines = text.map(x => x.split(' '));
 
+const p1 = lines.map(([a,b]) => {
 
-let p1 = p2 = 0;
+    const me = {'X': 1, 'Y': 2, 'Z': 3};
+    const scores = {
+        'AX': 3, 'AY': 6,
+        'BY': 3, 'BZ': 6,
+        'CX': 6, 'CZ': 3,
+    }
+    return me[b] + (scores[`${a}${b}`] || 0);
+ })
 
+const p2 = lines.map(([a,b]) => {
+    const me = {'X': 0, 'Y': 3, 'Z': 6};
+    const scores = {
+        'AX': 3, 'AY': 1, 'AZ': 2,
+        'BX': 1, 'BY': 2, 'BZ': 3,
+        'CX': 2, 'CY': 3, 'CZ': 1,
+    }
 
-console.log("Part 1 : ", p1);
-console.log("Part 2 : ", p2);
+    return  me[b] + scores[`${a}${b}`];
+})
+
+console.log("Part 1 : ", aoc.sum(p1)); // 14163
+console.log("Part 2 : ", aoc.sum(p2)); // 12091
