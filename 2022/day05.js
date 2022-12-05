ddfa29e26b6fc3a5f0f7d28b6262fbc023064af2
Paul Brown <paul.brown@krakenflex.com>
2022-12-05 06:24:15 +0000

    feat: day5

--- 2022/day05.js
+++ 2022/day05.js
@@ -10,26 +10,23 @@ const stacks = [
     'GLFQS',
     'VPWZBRCS',
     'ZFJ',
-    'DLVZRHQ' ,
-    'BHGNFZLD'];
+    'DLVZRHQ',
+    'BHGNFZLD']
 
 const moves = text.map(x => x.split(' '))
-     .map(x => ([x[1], x[3], x[5]]));
+     .map(x => ([x[1], x[3], x[5]]))
 
-const s1 = moves.reduce((a,[crates,from,to]) => {
-    const move = a[from-1].splice(-crates)
-    a[to-1] = a[to-1].concat(move.reverse());
-    return a;
-}, stacks.map(x => x.split('')));
-const p1 = s1.reduce((a,e) => a+=e[e.length-1], '');
+const arrange = (fn) => {
+     const s = moves.reduce((a,[crates,from,to]) => {
+        const move = a[from-1].splice(-crates)
+        a[to-1].push(...fn(move))
+        return a
+    }, stacks.map(x => x.split('')))
+    return s.reduce((a,e) => a+=e[e.length-1], '')
+}
 
-const s2 = moves.reduce((a,[crates,from,to]) => {
-    const move = a[from-1].splice(-crates)
-    a[to-1] = a[to-1].concat(move);
-    return a;
-}, stacks.map(x => x.split('')));
-const p2 = s2.reduce((a,e) => a+=e[e.length-1], '');
-
+const p1 = arrange((x) => x.reverse())
+const p2 = arrange((x) => x)
 
 console.log("Part 1 : ", (p1))
 console.log("Part 2 : ", (p2))
