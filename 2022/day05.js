6dd462da1022ba627a759cc6328fc58337cad7a5
Paul Brown <paul.brown@krakenflex.com>
2022-12-05 06:16:40 +0000

    feat: day5

--- 2022/day05.js
+++ 2022/day05.js
@@ -1,58 +1,35 @@
 const aoc = require('./aoc');
 const buffer = aoc.readfile('day05.txt');
-
 const text = buffer.split(/\n/);
-// const nums = text.map(x => parseInt(x));
-
-let a = 'HTZD'
-let b = 'QRWTGCS'
-let c = 'PBFQNRCH'
-let d = 'LCNFHZ'
-let e = 'GLFQS'
-let f = 'VPWZBRCS'
-let g = 'ZFJ'
-let h = 'DLVZRHQ' 
-let i  = 'BHGNFZLD'
-
-// move 2 from 1 to 9
-const moves = text.map(x => x.split(' '))
-     .map(x => ([x[1], x[3], x[5]]))
-
-console.log(moves)
 
 const stacks = [
-    a.split(''),
-    b.split(''),
-    c.split(''),
-    d.split(''),
-    e.split(''),
-    f.split(''),
-    g.split(''),
-    h.split(''),
-    i.split(''),
-    
+    'HTZD',
+    'QRWTGCS',
+    'PBFQNRCH',
+    'LCNFHZ',
+    'GLFQS',
+    'VPWZBRCS',
+    'ZFJ',
+    'DLVZRHQ' ,
+    'BHGNFZLD'];
 
-]
+const moves = text.map(x => x.split(' '))
+     .map(x => ([x[1], x[3], x[5]]));
 
-console.log(stacks)
-// move 2 from 1 to 9
-for (let i = 0; i < moves.length; i++) {
+const s1 = moves.reduce((a,[crates,from,to]) => {
+    const move = a[from-1].splice(-crates)
+    a[to-1] = a[to-1].concat(move.reverse());
+    return a;
+}, stacks.map(x => x.split('')));
+const p1 = s1.reduce((a,e) => a+=e[e.length-1], '');
 
-    for (let j = 0; j < moves[i][0]; j++) {
-        
-        const m = stacks[(moves[i][1])-1].splice(-1)
-        stacks[(moves[i][2])-1].push(m[0])
-        
-    }
-    
-//    RFFFBPNS
-//    RFFFWBPNS
-}
-// console.log("Part 1 : ", (p1)) // 431
-// console.log("Part 2 : ", (p2)) // 823
-let p1 =''
-for (let i = 0; i < stacks.length; i++) {
-    p1 += stacks[i][stacks[i].length -1]
-    
-}
-console.log(p1)
+const s2 = moves.reduce((a,[crates,from,to]) => {
+    const move = a[from-1].splice(-crates)
+    a[to-1] = a[to-1].concat(move);
+    return a;
+}, stacks.map(x => x.split('')));
+const p2 = s2.reduce((a,e) => a+=e[e.length-1], '');
+
+
+console.log("Part 1 : ", (p1))
+console.log("Part 2 : ", (p2))
