ae4bc25b56183f4ba4fea8f8f92cc3e782475436
Paul Brown <paul.brown@krakenflex.com>
2022-12-07 11:30:48 +0000

    feat: day7 tidy

--- 2022/day07.js
+++ 2022/day07.js
@@ -2,111 +2,74 @@ const aoc = require('./aoc');
 const buffer = aoc.readfile('day07.txt');
 const text = buffer.split(/\n/);
 
-root = { type: 'dir', name: '/', cnts: [], size: 0 }
+const buildTree = () => {
+    const root = { type: 'dir', name: '/', children: [], size: 0 }
+    const ptr = [root]
 
-ptr = [root]
-fs = ptr;
-
-for (let i = 1; i < text.length; i++) {
-    const cmd = text[i];
-    
-    
-
-    if (cmd.startsWith('$')) {
-        if (cmd.includes('cd')) {
-            if (cmd.split(' ')[2] === '..') {
+    for (let i = 1; i < text.length; i++) {
+        const cmd = text[i].split(' ');
+        if (cmd[1] === 'cd') {
+            if (cmd[2] === '..') {
                 ptr.pop()
             } else {
-                contents = ptr[ptr.length - 1].cnts
-                newdir = contents.find(x => x.name === cmd.split(' ')[2])
-                ptr.push(newdir)
+                ptr.push(ptr[ptr.length - 1].children.find(x => x.name === cmd[2]))
             }
         }
-    } else {
-        if (cmd.startsWith('dir')) {
-            newdir = { type: 'dir', size: 0, name: cmd.split(' ')[1], cnts: [] }
-            ptr[ptr.length - 1].cnts.push(newdir)
-        } else {
-            f = cmd.split(' ')
-            ptr[ptr.length - 1].cnts.push({ type: 'file', size: parseInt(f[0]), name: f[1] })
+
+        if (cmd[0] === 'dir') { // new dir
+            ptr[ptr.length - 1].children.push({ type: 'dir', size: 0, name: cmd[1], children: [] })
+        }
+        if (aoc.isNumber(cmd[0])) { // new file
+            ptr[ptr.length - 1].children.push({ type: 'file', size: parseInt(cmd[0]), name: cmd[1] })
         }
     }
 
-    // console.log('cmd', cmd)
-    // console.log('fs ', fs)
-    // console.log()
+    // Calculate dir sizes
+    const calc = (item) => {
+        if (item.type === 'dir') {
+            for (let i = 0; i < item.children.length; i++) {
+                item.size += calc(item.children[i])
+            }
+        }
+        return item.size
+    }
 
+    calc(root)
+    return root;
 }
 
+const tree = buildTree();
 
-final = fs[0]
-
-
-const calc = (dir) => {
+const sum = (dir, c) => {
     if (dir.type === 'dir') {
-        for (let i = 0; i < dir.cnts.length; i++) {
-            dir.size += calc(dir.cnts[i])
+        for (const child of dir.children) {
+            c = sum(child, c)
         }
-        return dir.size
-    } else {
-        console.log(dir.size)
-        return dir.size
+        if (dir.size <= 100000) c += dir.size
     }
-
-}
-calc(final)
-// console.log(JSON.stringify(final))
-
-
-s = 0
-const sum = (dir) => {
-    if (dir.type === 'dir') {
-        if (dir.size <= 100000) {
-            s += dir.size
-        }
-        for (let i = 0; i < dir.cnts.length; i++) {
-            sum(dir.cnts[i])
-
-        }
-    }
-}
-sum(final)
-p1 = s
-
-
-s1 = []
-const min = (dir) => {
-    if (dir.type === 'dir') {
-        s1.push(dir.size)
-        for (let i = 0; i < dir.cnts.length; i++) {
-            min(dir.cnts[i])
-        }
-    }
+    return c;
 }
 
+const p1 = sum(tree, 0);
 
-min(final)
 
+const disk = 70000000
+const required = 30000000
+const used = tree.size
+const free = disk - used
 
-total = s1[0];
-unused = 70000000 - total;
+const needToDelete = required - free
 
-need = 30000000
-console.log('unused ', unused)
-t = []
-for (let i = 0; i < s1.length; i++) {
-
-    free = s1[i] + unused
-
-    if (free >= need) {
-        t.push(s1[i])
+const scan = (item, min) => {
+    if (item.type === 'dir') {
+        localMin = item.size < min && item.size > needToDelete ? item.size : min
+        for (const children of item.children) {
+            min = scan(children, localMin)
+        }
     }
-
+    return min
 }
 
-console.log(Math.min(...t))
-
-
-
+const p2 = scan(tree, Infinity)
 console.log("Part 1 : ", (p1)) //1334506
-console.log("Part 2 : ", (Math.min(...t))) //7421137
+console.log("Part 2 : ", (p2)) //7421137
