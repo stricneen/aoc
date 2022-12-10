b32baf361e5846306ce8e4014de35129116fcf07
Paul Brown <paul.brown@krakenflex.com>
2022-12-10 07:37:46 +0000

    feat: day10

--- 2022/day10.js
+++ 2022/day10.js
@@ -1,101 +1,44 @@
-
 const aoc = require('./aoc');
-const buffer = aoc.readfile('day.txt');
+const buffer = aoc.readfile('day10.txt');
 const text = buffer.split(/\n/);
 
-const cmds = text.map(x => x.split(' ')).map(([x,y])=> [x,parseInt(y)] )
-cmds.push(...Array(100).fill(['noop']))
+const cmds = text.map(x => x.split(' ')).map(([x, y]) => [x, parseInt(y)])
+
+p1 = 0
 reg = 1
-c = []
+crt = []
 
-// console.log(cmds)
-
-let queue = []
-
-check = [20,60,100,140,180,220]
-// rn = []
+check = [20, 60, 100, 140, 180, 220]
 
 cycle = 1
-while(true) {
+ptr = 0
+curr = null
+while (ptr < cmds.length) {
 
-    if (cycle > 22) break;
+    const pixel = [reg - 1, reg, reg + 1].includes((cycle - 1) % 40) ? '\u2588' : ' '
+    crt.push(pixel)
 
-    cmd = cmds[cycle-1]
-    console.log(cycle, cmd)
-
-// 21,19,18,21,16
- 
-
-
-    queue = queue.map(([x,y]) => [x, y -1])
-    exec = queue.find(([x,y]) => y === 0)
-    queue = queue.filter(([x,y]) => y > 0)
-
-
-    if (cmd[0] === 'noop') {
-    }
-    if(cmd[0] === 'addx') {
-        queue.push([cmd[1], 1])
-    }
-    // console.log(queue)
-
-    // console.log(cycle, reg)
-
-    if (exec )  {
-        reg += exec[0]
-        console.log(cycle,'>',reg )
+    cmd = cmds[ptr]
+    if (curr === null) {
+        if (cmd[0] === 'noop') ptr++
+        if (cmd[0] === 'addx') curr = cmd[1]
+    } else {
+        ptr++
+        reg += curr
+        curr = null
     }
 
     if (check.includes(cycle)) {
-        console.log('-------------->',reg)
+        p1 += (reg * cycle)
     }
 
-    cycle ++
-
-// console.log()
-    
+    cycle++
 }
 
-// console.log(reg)
+console.log("Part 1 : ", (p1)) // 15120
+console.log("Part 2 : ", ('RKPJBPLA')) // RKPJBPLA
 
-// console.log(reg[19])
-// console.log(reg[20])
-// console.log(reg[21])
-
-// noop
-// noop
-// addx 3
-// noop
-// addx -21
-// addx 28
-// addx 1
-
-
-
-
-
-
-
-
-
-p1=p2=0
-console.log("Part 1 : ", (p1)) // 
-console.log("Part 2 : ", (p2)) // 
-
-
-// 2 addx 15
-// 3 addx -11
-// 4 addx 6
-// 5 addx -3
-// 6 addx 5
-// 7 addx -1
-// 8 addx -8
-// 9 addx 13
-// 10 addx 4
-// noop
-// addx -1
-// addx 5
-// addx -1
-// addx 5
-// addx -1
-// addx 5
+// draw screen
+for (let i = 0; i < 6; i++) {
+    console.log(crt.join('').slice(i * 40, (i * 40) + 40))
+}
