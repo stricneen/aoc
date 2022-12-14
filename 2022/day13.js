6c5b76bf4c870e2243d6f853f972baef32405a93
Paul Brown <paul.brown@krakenflex.com>
2022-12-14 20:45:45 +0000

    feat: day13 tidy

--- 2022/day13.js
+++ 2022/day13.js
@@ -3,78 +3,89 @@ const buffer = aoc.readfile('day.txt');
 const data = buffer.split(/\n\n/)
     .map(x => x.split(/\n/).map( y => JSON.parse(y)));
 
-const compare = (a, b) => {
-    console.log(a,'<>',b)
-    ca = a[0]
-    cb = b[0]
 
-    if (ca === undefined) return true
-    if (cb === undefined) return false
+// const compare2 = (a, b) => {
+//     console.log(a,'<>',b)
+//     if (Number.isInteger(a) && Array.isArray(b)) {
+//         a = [a]
+//     }
     
-    if (Number.isInteger(ca) && Number.isInteger(cb)) {
-        if (ca === cb) {
-            return compare(a.slice(1), b.slice(1))
-        }
-        return a < b
+//     if (Array.isArray(a) && Number.isInteger(b)) {
+//         b = [b]
+//     }
+    
+//     if (Number.isInteger(a) && Number.isInteger(b)) {
+//         if (a<b) return 1
+//         if (a===b) return 0
+//         return -1
+//     }
+    
+    
+//     if (Array.isArray(a) && Array.isArray(b)) {
+    
+//         i=0
+//         while (i < a.length && i < b.length){
+//             t = compare(a[i],b[i])
+//             console.log(i,a,b)
+//             if (t === 1) return 1
+//             if (t === -1) return -1
+//             i++
+//         }
+
+//         if (i === a.length) {
+//             if (a.length === b.length) {
+//                 return 0
+//             }
+//             return 1
+//         }
+//         return -1
+
+//     }
+
+
+//     // Testing lists
+
+// }
+
+const compare = (a, b, tab) => {
+    console.log(' '.repeat(tab * 2), a,'<>',b)
+
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (a<b) return 1
+        if (a>b) return -1
+        return 0
     }
 
-    if (Number.isInteger(ca) && Array.isArray(cb)) {
-        ca = [ca]
-    }
-    if (Array.isArray(ca) && Number.isInteger(cb)) {
-        cb = [cb]
-    }
+    if (Array.isArray(a) && Number.isInteger(b)) b = [b]
+    if (Array.isArray(b) && Number.isInteger(a)) a = [a]
 
-    const compareLists = (a,b) => {
-console.log('-------', a,b)
+    // TWO LISTS
+    min = Math.min(a.length, b.length)
+console.log('min', min)
+    for (let i = 0; i <= min; i++) {
+        console.log(' '.repeat(tab * 2), i, a, b)
+        n = compare(a[i], b[i], tab + 1)
 
-        if (Array.isArray(a[0]) && Array.isArray(b[0])){
-            return compareLists(a[0], b[0])
-        }
-
-
-        if (a.length === 0 && b.length === 0) return 0;
-
-        if (a.length > 0 && b.length > 0) {
-            if (a[0] < b[0]) return  1;
-        }
-
-        // if (b.length === 0) return -1;
-        if (a.length > 0 && b.length === 0)  return -1
-        // if (a.length === 0 && b.length > 0) return 1;
-
-    
-        
-
-        if (a.length === 0) return  1;
-        
-        if (a[0] > b[0]) return -1;
-        return compareLists(a.slice(1), b.slice(1))
+        if (n === 1) return 1
+        if (n === -1) return -1
     }
 
-
-    // Testing lists
-    console.log(ca,'<2>',cb)
-
-    const cl = compareLists(ca, cb)
-console.log('c1',cl)
-    if (cl === 1) return true;
-    if (cl === -1) return false;
-    return compare(a.slice(1), b.slice(1))
-    
-    
-    
+    console.log('wtf')
 
 }
+
 
 const index = []
 for (let i = 0; i < data.length; i++) {
-    c = compare(data[i][0], data[i][1])
+    c = compare(data[i][0], data[i][1] ,0)
     console.log('----------->',c)
-    if (c) {
+    if (c === 1) {
         index.push(i+1)
     }
 }
+
 console.log(index)
-console.log('Part 1', aoc.sum(index)); // 5623
+console.log('Part 1 : ', aoc.sum(index)); // 5623
 // console.log('Part 2', p2());   // 20570
+
+ // [1,2,4,6]
