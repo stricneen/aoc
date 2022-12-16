bc6a73e0f13aafbf18a1549bb9f3eebddc9b3ed0
Paul Brown <paul.brown@krakenflex.com>
2022-12-16 14:48:41 +0000

    feat: day16-1

--- 2022/day16.js
+++ 2022/day16.js
@@ -1,9 +1,68 @@
 const aoc = require('./aoc');
-const buffer = aoc.readfile('day.txt');
-const data = buffer.split(/\n/);
+const buffer = aoc.readfile('day16.txt');
+const map = buffer.split(/\n/)
+    .map(x => [x.split(' '), aoc.extractNums(x)])
+    .map(x => ({
+        valve: x[0][1], 
+        flow: x[1][0], 
+        open: false,
+        paths: x[0].slice(x[0].findIndex(y => y.startsWith('valve'))+1)
+            .map(x => x.replace(',','')) }))
+// console.log(map)
+//Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 
-console.log(data)
+let robots = [{at: 'AA', total: 0, open:[]}]
+for (let min = 0; min <= 29; min++) {
+    console.log(`--- Minute ${min}`)
+    // console.log(robots)
+    const next = []
+    for (const robot of robots) {
+        
+        robot.total += robot.open.reduce((a,e) => {
+            return a + map.find(x => x.valve === e).flow
+        },0)
+        
 
-p1 = p2 = 0
+        const loc = map.find(x => x.valve === robot.at);
+
+        if (!robot.open.includes(loc.valve) && loc.flow > 0) { // worth opening
+            next.push({...robot, open: robot.open.concat(loc.valve)})
+        } else { //move
+            for (const n of loc.paths) {
+                next.push({...robot, at: n})
+            }
+        }
+    }
+
+    robots = []
+    for (const loc of map) {
+     
+        
+        const robs = next.filter(x => x.at === loc.valve)
+        for (const v of robs) {
+            const n = robots.find(x => x.at === v.at && x.open.sort().join('') === v.open.sort().join(''))
+            if (n) {
+                if (n.total < v.total) n.total = v.total
+
+            } else {
+                robots.push(v)
+            }
+
+
+        }
+
+        
+    }
+    
+    // console.log(robots)
+    // console.log()
+
+   
+    
+}
+
+// console.log(robots.length)
+p1 = Math.max(...robots.map(x => x.total))
+p2 = 0
 console.log('Part 1 : ', p1); //     
 console.log('Part 2 : ', p2); // 
