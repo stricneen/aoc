4811f6aad2fe0b63558de4b80528611a363f27b4
Paul Brown <paul.brown@krakenflex.com>
2023-12-03 15:08:48 +0000

    feat: day3-2

--- 2023/day3.js
+++ 2023/day3.js
@@ -1,29 +1,18 @@
 const aoc = require('./aoc');
 const buffer = aoc.readfile('day3.txt');
 const textraw = buffer.split(/\n/);
-const text = ['.'.repeat(textraw[0].length), ...textraw.map(x => `.${x}.`),'.'.repeat(textraw[0].length)];
-// const nums = text.map(x=> [x,aoc.extractNums(x),[... new Set(aoc.extractNums(x))]]);
-// const x = nums.map(x => [x[1].length, x[2].length]).filter(x => x[0]!=x[1])
-// nums.map(x => [])
+const text = ['.'.repeat(textraw[0].length), ...textraw.map(x => `.${x}.`), '.'.repeat(textraw[0].length)];
 
-//  536147 h
-//  532286
-//  531225 l
-//  530358 l
-
-let p1 = 0;
+let p1a = 0;
+const gears = {};
 for (let i = 0; i < text.length; i++) {
-    
-    const a = text[i-1];
-    const l = text[i];
-    const b = text[i+1];
-    
-    const numsOnLine = aoc.extractStrictNums(l);
-    if (numsOnLine.length===0) continue;
-    // console.log(a,l,b)
 
-   
-    console.log(numsOnLine)
+    const a = text[i - 1];
+    const l = text[i];
+    const b = text[i + 1];
+
+    const numsOnLine = aoc.extractStrictNums(l);
+    if (numsOnLine.length === 0) continue;
 
     let pos = 0;
     for (let j = 0; j < numsOnLine.length; j++) {
@@ -32,25 +21,44 @@ for (let i = 0; i < text.length; i++) {
         const numLen = num.toString().length;
 
         p = l.indexOf(num, pos);
-        pos = p+numLen;
-        
-        const aa = a.substring(Math.max(p-1,0),Math.max(p-1,0)+numLen+2);
-        const ll = l.substring(Math.max(p-1,0),Math.max(p-1,0)+numLen+2);
-        const bb = b.substring(Math.max(p-1,0),Math.max(p-1,0)+numLen+2);
-        
-        const all = [...`${aa}${ll[0]}${ll[ll.length-1]}${bb}`].filter(c => c !=='.' && !aoc.isNumber(parseInt(c)))
-        
-    //  if (!all.length>0) console.log(aa,ll,bb,all.length>0)
-     //console.log(aa,ll,bb,all.length>0)
 
-     console.log(aa);console.log(ll);console.log(bb, all.length>0);console.log()
 
-     if (all.length>0) p1 += num;
+        const aa = a.substring(Math.max(p - 1, 0), Math.max(p - 1, 0) + numLen + 2);
+        const ll = l.substring(Math.max(p - 1, 0), Math.max(p - 1, 0) + numLen + 2);
+        const bb = b.substring(Math.max(p - 1, 0), Math.max(p - 1, 0) + numLen + 2);
+        const all = [...`${aa}${ll[0]}${ll[ll.length - 1]}${bb}`].filter(c => c !== '.' && !aoc.isNumber(parseInt(c)))
 
+        // console.log(aa);console.log(ll);console.log(bb, all.length>0);console.log();
+
+        if (all.length > 0) p1a += num;
+
+        // p2
+        if (all.includes('*')) {
+            if (aa.includes('*')) {
+                const key = `${i - 1}-${p + aa.indexOf('*')}`;
+                if (gears[key]) { gears[key].push(num) } else { gears[key] = [num] }
+            }
+            if (ll.includes('*')) {
+                const key = `${i}-${p + ll.indexOf('*')}`;
+                if (gears[key]) { gears[key].push(num) } else { gears[key] = [num] }
+            }
+            if (bb.includes('*')) {
+                const key = `${i + 1}-${p + bb.indexOf('*')}`;
+                if (gears[key]) { gears[key].push(num) } else { gears[key] = [num] }
+            }
+        }
+
+
+        pos = p + numLen;
     }
 }
 
-console.log(p1)
+let p2a = 0;
+Object.keys(gears).forEach(k => {
+    if (gears[k].length === 2) {
+        p2a += gears[k][0] * gears[k][1];
+    }
+})
 
-
-// console.log(text)
+console.log('Part 1: ', p1a); //532331
+console.log('Part 2: ', p2a); //82301120
