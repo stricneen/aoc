3f36413560494986711fa9c4ebf539ea0763a0d0
Paul Brown <paul.brown@krakenflex.com>
2023-12-05 11:37:16 +0000

    feat: day5-2

--- 2023/day5.js
+++ 2023/day5.js
@@ -4,22 +4,22 @@ const textraw = buffer.split(/\n/);
 
 let seeds = []
 let maps = []
-let locations  =[]
+
 textraw.forEach(l => {
     if (l.startsWith('seeds:')) { seeds = l.split(': ')[1].split(' ').map(x => parseInt(x)) }
 
-    else if (l === '') maps.push ({ ranges : []});
+    else if (l === '') maps.push({ ranges: [] });
 
     else if (l.endsWith('map:')) {
         to = l.split(' ')[0].split('-')
-        m = maps[maps.length-1]
+        m = maps[maps.length - 1]
         m.source = to[0]
-        m.dest = to[2]   
+        m.dest = to[2]
     }
 
     else {
         // console.log(l)
-        m = maps[maps.length-1]
+        m = maps[maps.length - 1]
         r = l.split(' ').map(x => parseInt(x))
         t = {}
         t.sourceStart = r[1]
@@ -28,65 +28,93 @@ textraw.forEach(l => {
         m.ranges.push(t)
     }
 });
+// 31161857
+// 31168506
 
-let p1a = Number.MAX_SAFE_INTEGER
+
+function* seedScanGen(seeds) {
+    for (let i = 0; i < seeds.length / 2; i++) {
+        const start = seeds[i * 2]
+        const range = seeds[(i * 2) + 1]
+        const step = Math.trunc(Math.sqrt(range));
+
+        for (let j = 0; j < range; j+=step) {
+            yield start + j;
+        }
+    }
+}
+
+function* seedReduceGen(seed) {
+
+    // 31161857 ***
+    // 31168506
+    
+    let s = seed
+    while(s > seed / 4) {
+        yield s;
+        s--;
+    }
+}
+
+
+function* single(seed) {
+    yield seed
+}
+
 let phaseNumber = 0
-for (let s = 0; s < seeds.length / 2; s++) {
-    let phaseName = 'seed'
-    phaseNumber = seeds[s];
+const calc = (seeds) => {
+    let final = { seed: 0, min :Number.MAX_SAFE_INTEGER }
+    for (const seed of seeds) {
 
-    while(true) {
-        console.log(phaseName, phaseNumber)
+        let phaseName = 'seed'
+        phaseNumber = seed;
 
-    const phase = maps.find(x => x.source === phaseName);
-    console.log(phase)
-    if (!phase) {
-        //done
-        console.log(phaseNumber)
-        console.log()
-        if (phaseNumber < p1a) p1a = phaseNumber;
-        locations.push(phaseNumber)
+        while (true) {
+            const phase = maps.find(x => x.source === phaseName);
+            if (!phase) {
+                if (phaseNumber < final.min) {
+                    final.min = phaseNumber;
+                    final.seed = seed;
+                }
+                break;
+            }
+
+            let found = false;
+            for (let r = 0; r < phase.ranges.length; r++) {
+                const p = phase.ranges[r];
+                if (phaseNumber >= p.sourceStart && phaseNumber < p.sourceStart + p.length) {
+                    found = true
+                    dest = p.destStart + phaseNumber - p.sourceStart
+                }
+            }
+            if (!found) dest = phaseNumber;
+
+            phaseNumber = dest
+            phaseName = phase.dest
+        }
+    }
+    return final
+}
+
+p1 = calc(seeds);
+console.log('Part 1:', p1.min)
+
+localmin = calc(seedScanGen(seeds))
+// console.log(localmin)
+
+s = localmin.seed
+// p2 = calc(seedReduceGen(localmin.seed))
+// console.log('Part 2: ', p2)
+lm = localmin.min
+
+while(true) {
+    x = calc(single(s))
+    // console.log(x)
+    if (x.min > lm) {
         break;
     }
-
-    let found = false;
-    for (let r = 0; r < phase.ranges.length; r++) {
-        const p = phase.ranges[r];
-
-        // const sources = range(p.sourceStart, p.sourceStart + p.length -1)
-        // const dests = range(p.destStart, p.destStart + p.length -1)
-        
-        if (phaseNumber >= p.sourceStart && phaseNumber < p.sourceStart + p.length) {
-            found = true
-            dest = p.destStart + phaseNumber - p.sourceStart
-        }
-
-        // console.log(sources, dests)
-
-        // const mappedSourceIndex = sources.findIndex(x => x === phaseNumber);
-        // if (mappedSourceIndex > -1) {
-        //     found = true;
-        //     dest = dests[mappedSourceIndex];
-        // }
-    }
-    if (!found) dest = phaseNumber;
-
-    phaseNumber = dest
-    phaseName = phase.dest
-    
-}
-// // seed-to-soil map:
-// 50 98 2
-// 52 50 48
-    
+    s--
+    lm = x.min
 }
 
-// console.log(seeds)
-// console.log(maps[0])
-// console.log(maps[0].ranges)
-
-console.log(locations)
-let p1 = 0, p2 = 0
-console.log('Part 1: ', Math.min(...locations))
-console.log('Part 2: ', p2);
-
+console.log('Part 2:', lm)
