faf3485143be06200c1b3854d440225c613c4c3f
Paul Brown <paul.brown@krakenflex.com>
2023-12-11 06:00:29 +0000

    feat: day11-2

--- 2023/day10.js
+++ 2023/day10.js
@@ -1,34 +1,40 @@
 const aoc = require('./aoc');
 const buffer = aoc.readfile('day10.txt');
-const textraw = buffer.split(/\n/);
+const inp = buffer.split(/\n/)
+
+const textraw = [
+    '.'.repeat(inp[0].length + 2),
+    ...inp.map(x => `.${x}.`),
+    '.'.repeat(inp[0].length + 2)
+];
 
 path = []
+
+const dup = [-1, 0]
+const ddown = [1, 0]
+const dleft = [0, -1]
+const dright = [0, 1]
+
 start = []
-
-const dup = [-1,0]
-const ddown = [1,0]
-const dleft = [0,-1]
-const dright = [0,1]
-
 textraw.forEach((r, i) => {
     if (r.indexOf('S') > -1) start = [i, r.indexOf('S')]
 })
 
 dir = []
-if ('-FL'.split('').includes(textraw[start[0]][start[1] - 1])) dir = [0, -1]
-if ('-7J'.split('').includes(textraw[start[0]][start[1] + 1])) dir = [0, 1]
-if ('|F7'.split('').includes(textraw[start[0] - 1][start[1]])) dir = [-1, 0]
-if ('|LJ'.split('').includes(textraw[start[0] + 1][start[1]])) dir = [1, 0]
+if ('-FL'.split('').includes(textraw[start[0]][start[1] - 1])) dir = dleft
+if ('-7J'.split('').includes(textraw[start[0]][start[1] + 1])) dir = dright
+if ('|F7'.split('').includes(textraw[start[0] - 1][start[1]])) dir = dup
+if ('|LJ'.split('').includes(textraw[start[0] + 1][start[1]])) dir = ddown
 
-console.log(start)
-console.log(dir)
+const vert = []
 
-path = []
 const trace = (pos, dir) => {
     npos = [pos[0] + dir[0], pos[1] + dir[1]]
     nfig = textraw[npos[0]][npos[1]]
 
-    if (nfig === 'S') return [0,0];
+    if ('|JL'.includes(nfig)) { vert.push(npos) }
+
+    if (nfig === 'S') return [0, 0];
 
     if (nfig === '|') ndir = dir
     if (nfig === '-') ndir = dir
@@ -48,47 +54,36 @@ const trace = (pos, dir) => {
     return [npos, ndir]
 }
 
-cc = trace(start, dir)
 
-s= 0
-p=start
-d=dir
+steps = 0
+p = start
+d = dir
 path = [p]
-// [ 36, 108 ]
 
 while (true) {
-    [np, nd] = trace(p,d)
-    s ++
-    if (aoc.eqArr([np,nd],[0,0])) break;
+    [np, nd] = trace(p, d)
+    steps++
+    if (aoc.eqArr([np, nd], [0, 0])) break;
     // console.log(np, nd,s)
-path.push(np)
-    p=np
-    d=nd
+    path.push(np)
+    p = np
+    d = nd
 }
-console.log(path[path.length-1])
-// console.log(s/2)
-// const histories = textraw.map(x => x.split(' ').map(y => parseInt(y)))
 
-// .....
-// .F-7.  F=S
-// .|.|.
-// .L-J.
-// .....
+inside = 0
+for (let i = 0; i < textraw.length; i++) {
+    for (let j = 0; j < textraw[i].length; j++) {
 
+        const onpath = path.some(x => x[0] === i && x[1] === j)
+        if (onpath) continue
 
-// | is a vertical pipe connecting north and south.
-// - is a horizontal pipe connecting east and west.
-// L is a 90-degree bend connecting north and east.
-// J is a 90-degree bend connecting north and west.
-// 7 is a 90-degree bend connecting south and west.
-// F is a 90-degree bend connecting south and east.
-// . is ground; there is no pipe in this tile.
-// S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+        const vr = vert.filter(x => x[0] === i && x[1] >= j)
 
+        if (vr.length % 2 === 1) {
+            inside++
+        }
+    }
+}
 
-// console.log(textraw)
-
-p1 = 0
-p2 = 0
-console.log('Part 1:', s/2); // 
-console.log('Part 2:', p2); // 
+console.log('Part 1:', steps / 2); // 6768
+console.log('Part 2:', inside); // 351
---
+++ 2023/day11.js
@@ -0,0 +1,46 @@
+const aoc = require('./aoc');
+const buffer = aoc.readfile('day11.txt');
+const textraw = buffer.split(/\n/);
+
+const galaxies = []
+expR = []
+expC = []
+
+for (let i = 0; i < textraw.length; i++) {
+    expC.push(i)
+}
+
+for (let i = 0; i < textraw.length; i++) {
+    if (textraw[i].indexOf('#') === -1) expR.push(i)
+    for (let j = 0; j < textraw[i].length; j++) {
+        const s = textraw[i][j];
+        if (s === '#') {
+            galaxies.push([i, j])
+            expC = expC.filter(x => x !== j)
+        }
+    }
+}
+
+const dists1 = []
+const dists2 = []
+
+for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+
+        const f = galaxies[i];
+        const t = galaxies[j];
+
+        const d = Math.abs(f[0] - t[0]) + Math.abs(f[1] - t[1])
+
+        const xc = expC.filter(x => Math.min(f[1], t[1]) < x && Math.max(f[1], t[1]) > x).length
+        const xr = expR.filter(x => Math.min(f[0], t[0]) < x && Math.max(f[0], t[0]) > x).length
+
+        d1 = d + xc + xr
+        d2 = d + (xr * (1000000-1)) + (xc * (1000000-1))
+        dists1.push(d1)
+        dists2.push(d2)
+    }
+}
+
+console.log('Part 1:', aoc.sum(dists1)); // 9648398
+console.log('Part 2:', aoc.sum(dists2)); // 618800410814
