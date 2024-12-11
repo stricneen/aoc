8121affc4b7a67b22a2e003b7defaae3b75eeead
Paul Brown <paul.brown@krakenflex.com>
2024-12-11 10:10:00 +0000

    chore: day12-s

---
+++ 2024/day12.js
@@ -0,0 +1,15 @@
+
+const { assert } = require('console');
+const aoc = require('./aoc');
+const buffer = aoc.readfilePro(12);
+// const text = buffer.split(' ').map(Number);
+// const text = buffer.split(/\n/);
+
+p1 = p2 = 0
+
+
+
+assert(p1 === 199982, 'p1')
+assert(p2 === 237149922829154, 'p2')
+console.log("Part 1 : ", p1);
+console.log("Part 2 : ", p2);
